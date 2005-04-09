828b586c0483c530adfe5510005eb49d0a90fcb8
rramthun <rramthun@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-04-09 15:30:38 +0000

    Added startYACY_hidden.bat
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@10 6c8d7289-2bf4-0310-a012-ef5d649a1542

---
+++ startYACY_hidden.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM Script contributed by Roland Ramthun.
+
+REM This script starts YaCy without any visible window and should work on all Windows-Operating-Systems which know the "start" command.
+REM You could verify this by typing "HELP" into cmd.exe, which lists all commands supported by your specific OS.
+
+start /B javaw -classpath classes yacy
+exit
