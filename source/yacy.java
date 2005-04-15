c13411c19800b5127413a79475a4e8fed3a718b4
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-04-15 13:50:42 +0000

    Buildfile which inserts the Date. The Version is set in the source; so it will be correct if you check old versions out.
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@21 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/yacy.java
+++ source/yacy.java
@@ -78,8 +78,8 @@ import de.anomic.yacy.*;
 public class yacy {
 
     // static objects
-    private static final String vString = "<<REPL_VERSION>>";
-    private static final String vDATE   = "<<REPL_DATE>>";
+    private static final String vString = "0.361";
+    private static final String vDATE   = "@REPL_DATE@";
     private static final String copyright = "[ YACY Proxy v" + vString + ", build " + vDATE + " by Michael Christen / www.yacy.net ]";
     private static final String hline = "-------------------------------------------------------------------------------";
     
