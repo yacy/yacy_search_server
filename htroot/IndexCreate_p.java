0a6cf3f5e74ab9b18a1142b5dfb5ef477521bc9f
(no author) <(no author)@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-04-19 06:33:53 +0000

    *) Bugfix: Reference to plasmaHTCache.Entry.urlString was not set correctly
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@23 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/IndexCreate_p.java
+++ htroot/IndexCreate_p.java
@@ -307,7 +307,7 @@ public class IndexCreate_p {
                             prop.put("indexing-queue_list_"+i+"_modified", daydate(pcentry.lastModified));
                             prop.put("indexing-queue_list_"+i+"_href",((pcentry.scraper == null) ? "0" : ("" + pcentry.scraper.getAnchors().size())));
                             prop.put("indexing-queue_list_"+i+"_anchor", ((pcentry.scraper == null) ? "-" : pcentry.scraper.getHeadline()) );
-                            prop.put("indexing-queue_list_"+i+"_url", pcentry.nomalizedURLString);
+                            prop.put("indexing-queue_list_"+i+"_url", pcentry.urlString);
                             dark = !dark;
                         }
                     }
