d5ff81c636c1b13fd50f794ab6df69ad700aa862
(no author) <(no author)@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-04-19 06:52:04 +0000

    *) Undoing last changes. Sorry.
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@25 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/htdocsdefault/dir.java
+++ htroot/htdocsdefault/dir.java
@@ -456,8 +456,7 @@ public class dir {
     public static void deletePhrase(plasmaSwitchboard switchboard, String urlstring, String phrase, String descr) {
         try {
             String urlhash = plasmaURL.urlHash(new URL(urlstring));
-            //Set words = plasmaCondenser.getWords(("yacyshare " + phrase + " " + descr).getBytes());
-            Set words = plasmaCondenser.getWords();
+            Set words = plasmaCondenser.getWords(("yacyshare " + phrase + " " + descr).getBytes());
             switchboard.removeReferences(urlhash, words);
             switchboard.loadedURL.remove(urlhash);
         } catch (Exception e) {
