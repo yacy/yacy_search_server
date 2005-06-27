a333715ffd6e7e16c60d0bede6428b518fa6d5b7
low012 <low012@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-06-27 19:52:40 +0000

    This changes eliminate a way to cripple Message pages on other peers by sending special messages with HTML tags. (See forum for details.)
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@325 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/Messages_p.java
+++ htroot/Messages_p.java
@@ -56,6 +56,7 @@ import de.anomic.plasma.plasmaSwitchboard;
 import de.anomic.server.serverFileUtils;
 import de.anomic.server.serverObjects;
 import de.anomic.server.serverSwitch;
+import de.anomic.tools.unHtml;
 
 public class Messages_p {
 
@@ -100,7 +101,7 @@ public class Messages_p {
                 messages += "<td>" + dateString(message.date()) + "</td>";
                 messages += "<td>" + message.author() + "</td>";
                 messages += "<td>" + message.recipient() + "</td>";
-                messages += "<td>" + message.subject() + "</td>";
+                messages += "<td>" + (new unHtml(message.subject())).un() + "</td>";
                 messages += "<td>" +
                             "<a href=\"Messages_p.html?action=view&object=" + key + "\">view</a>&nbsp;/&nbsp;" +
                             "<a href=\"MessageSend_p.html?hash=" + message.authorHash() + "&subject=Re: " + message.subject() + "\">reply</a>&nbsp;/&nbsp;" +
@@ -121,8 +122,8 @@ public class Messages_p {
             messages += "<tr><td class=\"MenuHeader\">From:</td><td class=\"MessageBackground\">" + message.author() + "</td></tr>";
             messages += "<tr><td class=\"MenuHeader\">To:</td><td class=\"MessageBackground\">" + message.recipient() + "</td></tr>";
             messages += "<tr><td class=\"MenuHeader\">Send Date:</td><td class=\"MessageBackground\">" + dateString(message.date()) + "</td></tr>";
-            messages += "<tr><td class=\"MenuHeader\">Subject:</td><td class=\"MessageBackground\">" + message.subject() + "</td></tr>";
-            messages += "<tr><td class=\"MessageBackground\" colspan=\"2\">" + new String(message.message()) + "</td></tr>";
+            messages += "<tr><td class=\"MenuHeader\">Subject:</td><td class=\"MessageBackground\">" + (new unHtml(message.subject())).un() + "</td></tr>";
+            messages += "<tr><td class=\"MessageBackground\" colspan=\"2\">" + (new unHtml(new String(message.message()))).un() + "</td></tr>";
 	    messages += "<tr><td class=\"MenuHeader\">Action:</td>" +	    
 	    		"<td class=\"MessageBackground\">" +
 			"<a href=\"Messages_p.html\">inbox</a>&nbsp;/&nbsp;" +
