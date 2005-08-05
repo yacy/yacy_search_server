0211c4f2977b404c3b199ef47f27a8fc9dc18160
rramthun <rramthun@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-08-05 18:18:50 +0000

    Added scripts for Windows ME and 98 as requested in http://www.yacy-forum.de/viewtopic.php?t=839.
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@494 6c8d7289-2bf4-0310-a012-ef5d649a1542

---
+++ startYACY_noconsole_Win9x.bat
@@ -0,0 +1,21 @@
+@Echo Off
+If %1.==CPGEN. GoTo :CPGEN
+
+Rem Generating the proper classpath unsing loops and labels
+Set CLASSPATH=classes;htroot
+For %%X in (lib\*.jar) Do Call %0 CPGEN %%X
+For %%X in (libx\*.jar) Do Call %0 CPGEN %%X
+
+Rem Starting yacy
+Echo Generated Classpath:%CLASSPATH%
+javaw -classpath %CLASSPATH% yacy
+Echo You can close the console safely
+
+GoTo :END
+
+Rem This target is used to concatenate the classpath parts
+:CPGEN
+Set CLASSPATH=%CLASSPATH%;%2
+
+Rem Target needed to jump to the end of the file
+:END
