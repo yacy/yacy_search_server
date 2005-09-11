595ec6faf1643490df485b634549f2f827de6e69
borg-0300 <borg-0300@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-09-11 21:03:35 +0000

    svn keyword test
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@706 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/Blacklist_p.java
+++ htroot/Blacklist_p.java
@@ -7,9 +7,9 @@
 //
 // This File is contributed by Alexander Schier
 //
-// $Date$
-// $Author$
-// $Revision$
+// $LastChangedDate$
+// $LastChangedRevision$
+// $LastChangedBy$
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
