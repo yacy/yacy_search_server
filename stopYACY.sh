211a72ec6bb2f79368654259bf12826a2b4ec904
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-09-21 20:08:40 +0000

    using wget instead of java for shutdown -better performance -more native
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@765 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- stopYACY.sh
+++ stopYACY.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 cd `dirname $0`
 
-# generating the proper classpath
-CLASSPATH=""
-for N in `ls -1 lib/*.jar`; do CLASSPATH="$CLASSPATH$N:"; done	
-for N in `ls -1 libx/*.jar`; do CLASSPATH="$CLASSPATH$N:"; done
+## generating the proper classpath
+#CLASSPATH=""
+#for N in `ls -1 lib/*.jar`; do CLASSPATH="$CLASSPATH$N:"; done	
+#for N in `ls -1 libx/*.jar`; do CLASSPATH="$CLASSPATH$N:"; done
 
-java -classpath classes:htroot:$CLASSPATH yacy -shutdown
+#java -classpath classes:htroot:$CLASSPATH yacy -shutdown
+
+port=`cat DATA/SETTINGS/httpProxy.conf |grep "^port="|sed "s/.*=//"`
+pw=`cat DATA/SETTINGS/httpProxy.conf |grep "^adminAccountBase64MD5="|sed "s/.*=//"`
+wget -q --header "Authorization: realm=$pw" http://localhost:$port/Steering.html?shutdown=true -O /dev/null
+
 echo "please wait until the YaCy daemon process terminates"
 echo "you can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
