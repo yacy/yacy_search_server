2c5999ae004e837e2decb878da3732e6a8988e2e
hydrox <hydrox@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2005-11-03 10:44:53 +0000

    *)fixed UNRESOLVED PATTERN in ViewLog_p.html
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1020 6c8d7289-2bf4-0310-a012-ef5d649a1542

---
+++ htroot/ViewLog_p.java
@@ -0,0 +1,9 @@
+import de.anomic.http.httpHeader;
+import de.anomic.server.serverObjects;
+import de.anomic.server.serverSwitch;
+
+public class ViewLog_p {
+	public static serverObjects respond(httpHeader header, serverObjects post, serverSwitch env) {
+		return null;
+	}
+}
