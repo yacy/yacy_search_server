21fac0b6da836818b65bae1777f792931451a856
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-01-11 00:43:00 +0000

    small bugfix
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1310 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/yacy.java
+++ source/yacy.java
@@ -1278,17 +1278,14 @@ public final class yacy {
             if (resource.equals("all")) {
                 plasmaWordIndex WordIndex = new plasmaWordIndex(homeDBroot, 8*1024*1024, log);
                 WordHashIterator = WordIndex.wordHashes(wordChunkStartHash, true, false);
-            }
-            if (resource.equals("assortments")) {
+            } else if (resource.equals("assortments")) {
                 plasmaWordIndexAssortmentCluster assortmentCluster = new plasmaWordIndexAssortmentCluster(new File(homeDBroot, "ACLUSTER"), 64, 16*1024*1024, log);
                 WordHashIterator = assortmentCluster.hashConjunction(wordChunkStartHash, true);
-            }
-            if (resource.startsWith("assortment")) {
+            } else if (resource.startsWith("assortment")) {
                 int a = Integer.parseInt(resource.substring(10));
                 plasmaWordIndexAssortment assortment = new plasmaWordIndexAssortment(new File(homeDBroot, "ACLUSTER"), a, 8*1024*1024, null);
                 WordHashIterator = assortment.hashes(wordChunkStartHash, true, false);
-            }
-            if (resource.equals("words")) {
+            } else if (resource.equals("words")) {
                 plasmaWordIndexClassicDB fileDB = new plasmaWordIndexClassicDB(homeDBroot, log);
                 WordHashIterator = fileDB.wordHashes(wordChunkStartHash, true);
             }
