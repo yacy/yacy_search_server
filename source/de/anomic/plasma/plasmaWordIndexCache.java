214302284e25f04ea1e7b343eadacb5dcf7efc21
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-02-02 10:05:16 +0000

    *) undoing last commit because of problems with getUpdateTime
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1514 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/plasma/plasmaWordIndexCache.java
+++ source/de/anomic/plasma/plasmaWordIndexCache.java
@@ -364,11 +364,13 @@ public final class plasmaWordIndexCache implements plasmaWordIndexInterface {
         plasmaWordIndexEntryContainer container = null;
         long time;
         synchronized (cache) {
-            // get the container and remove it from the cache
-            container = (plasmaWordIndexEntryContainer) this.cache.remove(key);
+            // get the container
+            container = (plasmaWordIndexEntryContainer) this.cache.get(key);
             if (container == null) return 0; // flushing of nonexisting key
             time = getUpdateTime(key);
 
+            // remove it from the cache
+            cache.remove(key);
             hashScore.deleteScore(key);
             hashDate.deleteScore(key);
         }
