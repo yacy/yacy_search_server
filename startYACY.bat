0ff0e444ac922289c9605d4fb1b51764c57a83ed
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-02-02 15:54:10 +0000

    *) startYACY.bat now reads the java runtime memory settings out of the yacy config file    Thanks to Galaxis and VT100
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1515 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- startYACY.bat
+++ startYACY.bat
@@ -9,11 +9,27 @@ Set CLASSPATH=classes;htroot
 For %%X in (lib/*.jar) Do Call %0 CPGEN lib\%%X
 For %%X in (libx/*.jar) Do Call %0 CPGEN libx\%%X
 
+set javacmd=-Xmx64m -Xms10m
+if exist DATA\SETTINGS\httpProxy.conf GoTo :GETJAVACMD
+
+:STARTJAVA
 Rem Starting yacy
 Echo Generated Classpath:%CLASSPATH%
-java -classpath %CLASSPATH% yacy
+Echo JRE Parameters:%javacmd%
+java %javacmd% -classpath %CLASSPATH% yacy
 
 GoTo :END
+
+Rem This target is used to read java runtime parameters out of the yacy config file
+:GETJAVACMD
+for /F "tokens=1,2 delims==" %%i in (DATA\SETTINGS\httpProxy.conf) do (
+	if "%%i"=="javastart_Xmx" set jmx=%%j
+	if "%%i"=="javastart_Xms" set jms=%%j
+)
+if defined jmx set javacmd=-%jmx%
+if defined jms set javacmd=-%jms% %javacmd%
+
+GoTo :STARTJAVA
 
 Rem This target is used to concatenate the classpath parts 
 :CPGEN
