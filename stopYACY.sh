1eab543254483d8fa12a9141a6f79c1256d32535
rramthun <rramthun@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-02-04 19:31:12 +0000

    Updated German language Updated stopYACY.sh
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1535 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- stopYACY.sh
+++ stopYACY.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 cd `dirname $0`
 
-if [ -x `which wget` ];
+if [ -x `which wget` ]
 then
 port=`cat DATA/SETTINGS/httpProxy.conf |grep "^port="|sed "s/.*=//"`
 pw=`cat DATA/SETTINGS/httpProxy.conf |grep "^adminAccountBase64MD5="|sed "s/.*=//"`
 wget -q --header "Authorization: realm=$pw" http://localhost:$port/Steering.html?shutdown=true -O /dev/null
-else
+
+echo "Please wait until the YaCy daemon process terminates"
+echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
+
+elif [ -x `which java` ]
 
 # generating the proper classpath
 CLASSPATH=""
@@ -14,7 +18,11 @@ for N in lib/*.jar; do CLASSPATH="$CLASSPATH$N:"; done
 for N in libx/*.jar; do CLASSPATH="$CLASSPATH$N:"; done
 
 java -classpath classes:htroot:$CLASSPATH yacy -shutdown
-fi
 
 echo "Please wait until the YaCy daemon process terminates"
 echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
+
+else
+echo "One of the following programs is not installed or not executable: wget, cat, sed or grep."
+echo "YaCy could not be stopped. Please use your webbrowser to do so."
+fi
