883f03c3992824151fac4047e86c3ed3002472af
rramthun <rramthun@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-02-04 19:38:44 +0000

    I will never change the Linux scripts...
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1536 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- stopYACY.sh
+++ stopYACY.sh
@@ -11,7 +11,7 @@ echo "Please wait until the YaCy daemon process terminates"
 echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
 
 elif [ -x `which java` ]
-
+then
 # generating the proper classpath
 CLASSPATH=""
 for N in lib/*.jar; do CLASSPATH="$CLASSPATH$N:"; done	
