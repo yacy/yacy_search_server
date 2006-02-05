512b3f9b948ed03cd39ae814feeebbfb901839e3
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-02-05 13:13:51 +0000

    retrylimit for wget and better errormessage when neither wget nor java could be found.
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1542 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- stopYACY.sh
+++ stopYACY.sh
@@ -3,26 +3,26 @@ cd `dirname $0`
 
 if [ -x `which wget` ]
 then
-port=`cat DATA/SETTINGS/httpProxy.conf |grep "^port="|sed "s/.*=//"`
-pw=`cat DATA/SETTINGS/httpProxy.conf |grep "^adminAccountBase64MD5="|sed "s/.*=//"`
-wget -q --header "Authorization: realm=$pw" http://localhost:$port/Steering.html?shutdown=true -O /dev/null
+	port=`cat DATA/SETTINGS/httpProxy.conf |grep "^port="|sed "s/.*=//"`
+	pw=`cat DATA/SETTINGS/httpProxy.conf |grep "^adminAccountBase64MD5="|sed "s/.*=//"`
+	wget -q -t 1 --header "Authorization: realm=$pw" http://localhost:$port/Steering.html?shutdown=true -O /dev/null
 
 echo "Please wait until the YaCy daemon process terminates"
 echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
 
 elif [ -x `which java` ]
 then
-# generating the proper classpath
-CLASSPATH=""
-for N in lib/*.jar; do CLASSPATH="$CLASSPATH$N:"; done	
-for N in libx/*.jar; do CLASSPATH="$CLASSPATH$N:"; done
+	# generating the proper classpath
+	CLASSPATH=""
+	for N in lib/*.jar; do CLASSPATH="$CLASSPATH$N:"; done	
+	for N in libx/*.jar; do CLASSPATH="$CLASSPATH$N:"; done
 
-java -classpath classes:htroot:$CLASSPATH yacy -shutdown
+	java -classpath classes:htroot:$CLASSPATH yacy -shutdown
 
-echo "Please wait until the YaCy daemon process terminates"
-echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
+	echo "Please wait until the YaCy daemon process terminates"
+	echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
 
 else
-echo "One of the following programs is not installed or not executable: wget, cat, sed or grep."
-echo "YaCy could not be stopped. Please use your webbrowser to do so."
+	echo "Neither wget nor java could be found."
+	echo "visit http://localhost:8080/Steering.html?shutdown=true to stop YaCy or use ./killYACY.sh"
 fi
