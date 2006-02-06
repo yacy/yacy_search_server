62a0bb475ab7a580d69b9514d436ce04167f9343
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-02-06 19:47:59 +0000

    More values displayed on WatchCrawler.html status_p.xml: to be extended.
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1561 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/js/WatchCrawler.js
+++ htroot/js/WatchCrawler.js
@@ -1,78 +1,102 @@
 DELETE_STRING="delete"
-function handleResponse(){
-    if(http.readyState == 4){
-        var response = http.responseXML;
-        indexingTable=document.getElementById("indexingTable");
-		if(response != null){
-	        entries=response.getElementsByTagName("entry");
-		}
+
+var statusRPC;
+function requestStatus(){
+	statusRPC=createRequestObject()
+	statusRPC.open('get', '/xml/status_p.xml');
+	statusRPC.onreadystatechange = handleStatus;
+	statusRPC.send(null)
+}
+function requestIndexingQueue(){
+	indexingQueueRPC=createRequestObject()
+	indexingQueueRPC.open('get', '/xml/queues/indexing_p.xml');
+	indexingQueueRPC.onreadystatechange = handleIndexingQueue;
+	indexingQueueRPC.send(null);
+
+}
+window.setInterval("requestStatus()", 5000);
+window.setInterval("requestIndexingQueue()", 5000);
+
+
+
+//window.setInterval("sndReq('/xml/queues/indexing_p.xml')", 5000);
+
+
+function handleStatus(){
+	var statusResponse = statusRPC.responseXML;
+	indexingqueue=statusResponse.getElementsByTagName("indexingqueue")[0];
+	indexingqueue_size=indexingqueue.firstChild.nextSibling;
+	indexingqueue_max=indexingqueue_size.nextSibling.nextSibling;
+	ppm=statusResponse.getElementsByTagName("ppm")[0];
+	document.getElementById("indexingqueuesize").firstChild.nodeValue=indexingqueue_size.firstChild.nodeValue;
+	document.getElementById("indexingqueuemax").firstChild.nodeValue=indexingqueue_max.firstChild.nodeValue;
+	document.getElementById("ppm").firstChild.nodeValue=ppm.firstChild.nodeValue;
+}
+function handleIndexingQueue(){
+	var indexingQueueResponse = indexingQueueRPC.responseXML;
+    indexingTable=document.getElementById("indexingTable");
+	if(indexingQueueResponse != null){
+	    entries=indexingQueueResponse.getElementsByTagName("entry");
+	}
     
-        //skip the Tableheade
+    //skip the Tableheade
+    row=indexingTable.firstChild.nextSibling.firstChild.nextSibling.nextSibling;
+
+    while(row != null){ //delete old entries
+        indexingTable.firstChild.nextSibling.removeChild(row);
         row=indexingTable.firstChild.nextSibling.firstChild.nextSibling.nextSibling;
-
-        while(row != null){ //delete old entries
-            indexingTable.firstChild.nextSibling.removeChild(row);
-            row=indexingTable.firstChild.nextSibling.firstChild.nextSibling.nextSibling;
-        }
+    }
         
-        dark=false;
-        for(i=0;i<entries.length;i++){
-			initiator="";
-			depth="";
-			modified="";
-			anchor="";
-			url="";
-			size="";
-			hash="";
-			inProcess=false;
-            
-            //simply add all fields chronologically
-            //TODO: add them by Name
-            field=entries[i].firstChild;
-            while(field != null){
-                if(field.nodeType == 1 && field.firstChild!=null){//Element
-					if(field.nodeName=="initiator"){
-						initiator=field.firstChild.nodeValue;
-					}else if(field.nodeName=="depth"){
-						depth=field.firstChild.nodeValue;
-					}else if(field.nodeName=="modified"){
-						modified=field.firstChild.nodeValue;
-					}else if(field.nodeName=="anchor"){
-						anchor=field.firstChild.nodeValue;
-					}else if(field.nodeName=="url"){
-						url=field.firstChild.nodeValue;
-					}else if(field.nodeName=="size"){
-						size=field.firstChild.nodeValue;
-					}else if(field.nodeName=="hash"){
-						hash=field.firstChild.nodeValue;
-					}else if(field.nodeName=="inProcess"){
-						if(field.firstChild.nodeValue=="true"){
-							inProcess=true;
-						}
+    dark=false;
+    for(i=0;i<entries.length;i++){
+		initiator="";
+		depth="";
+		modified="";
+		anchor="";
+		url="";
+		size="";
+		hash="";
+		inProcess=false;
+           
+        field=entries[i].firstChild;
+        while(field != null){
+            if(field.nodeType == 1 && field.firstChild!=null){//Element
+				if(field.nodeName=="initiator"){
+					initiator=field.firstChild.nodeValue;
+				}else if(field.nodeName=="depth"){
+					depth=field.firstChild.nodeValue;
+				}else if(field.nodeName=="modified"){
+					modified=field.firstChild.nodeValue;
+				}else if(field.nodeName=="anchor"){
+					anchor=field.firstChild.nodeValue;
+				}else if(field.nodeName=="url"){
+					url=field.firstChild.nodeValue;
+				}else if(field.nodeName=="size"){
+					size=field.firstChild.nodeValue;
+				}else if(field.nodeName=="hash"){
+					hash=field.firstChild.nodeValue;
+				}else if(field.nodeName=="inProcess"){
+					if(field.firstChild.nodeValue=="true"){
+						inProcess=true;
 					}
-                }
-                field=field.nextSibling;
+				}
             }
-			row=createRow(initiator, depth, modified, anchor, url, size, hash);
-			//create row
-            /*col=document.createElement("td");
-            text=document.createTextNode(initiator);
-            col.appendChild(text);
-            row.appendChild(col);*/
-
-			if(inProcess){
-                row.setAttribute("class", "TableCellSummary");
-            }else if(dark){
-                row.setAttribute("class", "TableCellDark");
-            }else{
-                row.setAttribute("class", "TableCellLight");
-            }
-            indexingTable.firstChild.nextSibling.appendChild(row);
-            dark=!dark;
+            field=field.nextSibling;
         }
+		row=createRow(initiator, depth, modified, anchor, url, size, hash);
+		//create row
+		if(inProcess){
+            row.setAttribute("class", "TableCellActive");
+        }else if(dark){
+            row.setAttribute("class", "TableCellDark");
+        }else{
+            row.setAttribute("class", "TableCellLight");
+        }
+        indexingTable.firstChild.nextSibling.appendChild(row);
+        dark=!dark;
     }
 }
-window.setInterval("sndReq('/xml/queues/indexing_p.xml')", 5000);
+
 
 function createCol(content){
 	col=document.createElement("td");
