2635244489e8cfd921ebe8cb7addbe7d20a401cd
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-03-18 14:08:34 +0000

    Bugfix from cato
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@1924 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- addon/yacy-xchat-plugin.py
+++ addon/yacy-xchat-plugin.py
@@ -14,5 +14,6 @@ def yacy_ppm(word, word_eol, userdata):
   for line in urllib.urlopen("http://"+user+":"+password+"@"+host+"/xml/status_p.xml").readlines():
     if re.compile("<ppm>").search(line):
       xchat.command("me 's YaCy is crawling at "+line.strip().strip("<ppm/>")+" pages per minute.")
+  return xchat.EAT_ALL
       
 xchat.hook_command("YACY_SHOW",yacy_ppm,help="/yacy_show - shows the current ppm")      
