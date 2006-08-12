248627ab925213c4e1bcae2753d2a76e577e0cec
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-08-12 17:59:13 +0000

    added open search compatibility
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@2401 6c8d7289-2bf4-0310-a012-ef5d649a1542

---
+++ htroot/opensearchdescription.java
@@ -0,0 +1,53 @@
+// opensearchdescription.java
+// (C) 2006 by Michael Peter Christen; mc@anomic.de, Frankfurt a. M., Germany
+// first published 12.08.2006 on http://www.anomic.de
+//
+// This is a part of YaCy, a peer-to-peer based web search engine
+//
+// $LastChangedDate: 2006-04-02 22:40:07 +0200 (So, 02 Apr 2006) $
+// $LastChangedRevision: 1986 $
+// $LastChangedBy: orbiter $
+//
+// LICENSE
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import de.anomic.http.httpHeader;
+import de.anomic.server.serverCore;
+import de.anomic.server.serverObjects;
+import de.anomic.server.serverSwitch;
+import de.anomic.yacy.yacyCore;
+
+public class opensearchdescription {
+
+    public static serverObjects respond(httpHeader header, serverObjects post, serverSwitch env) {
+
+        // generate message content for open search description
+        String promoteSearchPageGreeting = env.getConfig("promoteSearchPageGreeting", "");
+        if (promoteSearchPageGreeting.length() == 0) promoteSearchPageGreeting = "P2P WEB SEARCH";
+
+        String thisaddress = (String) header.get("Host", "localhost");
+        if (thisaddress.indexOf(":") == -1) thisaddress += ":" + serverCore.getPortNr(env.getConfig("port", "8080"));
+        
+        final serverObjects prop = new serverObjects();
+        prop.put("thisaddress", thisaddress);
+        prop.put("SearchPageGreeting", promoteSearchPageGreeting);
+        prop.put("clientname", yacyCore.seedDB.mySeed.getName());
+        
+        // return rewrite properties
+        return prop;
+    }
+
+}
