68204ff729308b2440f9e670395ede7c942b23ba
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-10-19 11:10:56 +0000

    *) Suppressing for bad client requests.    See: http://www.yacy-forum.de/viewtopic.php?p=26918
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@2814 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/http/httpd.java
+++ source/de/anomic/http/httpd.java
@@ -473,6 +473,8 @@ public final class httpd implements serverHandler {
                 } else if ((errorMsg.startsWith("Broken pipe") || errorMsg.startsWith("Connection reset"))) {
                     // client closed the connection, so we just end silently
                     this.log.logInfo("Client unexpectedly closed connection");
+                } else if (errorMsg.equals("400 Bad request")) {
+                	this.log.logInfo("Bad client request.");
                 } else {
                     this.log.logSevere("Unexpected Error. " + e.getClass().getName() + ": " + e.getMessage(),e);
                 }
