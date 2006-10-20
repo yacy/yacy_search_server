a17c43779f8b6d48485137811d20dc078b62885f
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-10-20 14:50:57 +0000

    removed wrong part of template
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@2830 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/xml/blacklists_p.java
+++ htroot/xml/blacklists_p.java
@@ -3,9 +3,6 @@
 // (C) 2006 Alexander Schier
 // part of YaCy
 //
-// last major change: 06.02.2006
-// this file is contributed by Alexander Schier
-//
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
