8bdf22f325b27056420344142cf6b45a9bdded1b
theli <theli@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-10-28 13:07:17 +0000

    *) addapting test class to new function name
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@2873 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- test/ParseVersion.java
+++ test/ParseVersion.java
@@ -8,26 +8,26 @@ public class ParseVersion extends TestCase {
 	 * @link <a href="http://www.yacy-forum.de/viewtopic.php?t=2717">yacy-forum.de: ne Verbesserung von combinedVersionString2PrettyString(...)</a>
 	 */
 	public void testCombinedVersionString2PrettyString() {
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString(""));                 // not a number
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString(" "));                // not a number
-        assertEquals("dev/02417",       yacy.combinedVersionString2PrettyString("0.10002417"));
-        assertEquals("dev/02440",       yacy.combinedVersionString2PrettyString("0.10002440"));
-        assertEquals("dev/02417",       yacy.combinedVersionString2PrettyString("0.10002417"));
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("0.100024400"));      // input is too long
-        assertEquals("dev/02440",       yacy.combinedVersionString2PrettyString("0.10902440"));
-        assertEquals("0.110/02440",     yacy.combinedVersionString2PrettyString("0.11002440"));
-        assertEquals("0.111/02440",     yacy.combinedVersionString2PrettyString("0.11102440"));    
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("0.00000000"));       // input is valid - no warning generated
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("    0.11102440"));   // spaces are not allowed
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("0.111244"));         // input is too short
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("0.1112440\t\n"));    // \t and \n are not allowed
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("124353432xxxx4546399999"));  // not a number + too long 
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("123456789x"));       // not a number
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("9999999999"));       // missing decimal point
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("999.999999"));       // floating point part must have 3 and SVN-Version 5 digits
-        assertEquals("0.999/99999",     yacy.combinedVersionString2PrettyString("0.99999999"));    
-        assertEquals("99999.004/56789", yacy.combinedVersionString2PrettyString("99999.00456789"));    
-        assertEquals("dev/00000",       yacy.combinedVersionString2PrettyString("99999.003456789"));  // input is too long
+        assertEquals("dev/00000",       yacy.combined2prettyVersion(""));                 // not a number
+        assertEquals("dev/00000",       yacy.combined2prettyVersion(" "));                // not a number
+        assertEquals("dev/02417",       yacy.combined2prettyVersion("0.10002417"));
+        assertEquals("dev/02440",       yacy.combined2prettyVersion("0.10002440"));
+        assertEquals("dev/02417",       yacy.combined2prettyVersion("0.10002417"));
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("0.100024400"));      // input is too long
+        assertEquals("dev/02440",       yacy.combined2prettyVersion("0.10902440"));
+        assertEquals("0.110/02440",     yacy.combined2prettyVersion("0.11002440"));
+        assertEquals("0.111/02440",     yacy.combined2prettyVersion("0.11102440"));    
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("0.00000000"));       // input is valid - no warning generated
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("    0.11102440"));   // spaces are not allowed
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("0.111244"));         // input is too short
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("0.1112440\t\n"));    // \t and \n are not allowed
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("124353432xxxx4546399999"));  // not a number + too long 
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("123456789x"));       // not a number
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("9999999999"));       // missing decimal point
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("999.999999"));       // floating point part must have 3 and SVN-Version 5 digits
+        assertEquals("0.999/99999",     yacy.combined2prettyVersion("0.99999999"));    
+        assertEquals("99999.004/56789", yacy.combined2prettyVersion("99999.00456789"));    
+        assertEquals("dev/00000",       yacy.combined2prettyVersion("99999.003456789"));  // input is too long
     }
 
 }
