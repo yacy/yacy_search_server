ebb42906f8546f1f30c16d5b83ca4e58c5578ae3
hydrox <hydrox@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2006-11-30 07:33:54 +0000

    *) removed special characters *) added Copyright comments
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@3026 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/server/logging/LogalizerHandler.java
+++ source/de/anomic/server/logging/LogalizerHandler.java
@@ -1,3 +1,47 @@
+//LogalizerHandler.java 
+//-------------------------------------
+//part of YACY
+//(C) by Michael Peter Christen; mc@anomic.de
+//first published on http://www.anomic.de
+//Frankfurt, Germany, 2004
+//
+//This file ist contributed by Matthias Soehnholz
+//last major change: $LastChangedDate$ by $LastChangedBy$
+//Revision: $LastChangedRevision$
+//
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//Using this software in any meaning (reading, learning, copying, compiling,
+//running) means that you agree that the Author(s) is (are) not responsible
+//for cost, loss of data or any harm that may be caused directly or indirectly
+//by usage of this softare or this documentation. The usage of this software
+//is on your own risk. The installation and usage (starting/running) of this
+//software may allow other people or application to access your computer and
+//any attached devices and is highly dependent on the configuration of the
+//software which must be done by the user of the software; the author(s) is
+//(are) also not responsible for proper configuration and usage of the
+//software, even if provoked by documentation provided together with
+//the software.
+//
+//Any changes to this file according to the GPL as documented in the file
+//gpl.txt aside this file in the shipment you received can be done to the
+//lines that follows this copyright notice here, but changes must not be
+//done inside the copyright notive above. A re-distribution must contain
+//the intact and unchanged copyright notice.
+//Contributions and changes to the program code must be marked as such.
+
 package de.anomic.server.logging;
 
 import java.io.File;
@@ -27,10 +71,10 @@ public class LogalizerHandler extends Handler {
     private HashMap loadParsers() {
         HashMap parsers = new HashMap();
         try {
-            if (enabled) System.out.println("Searching for additional content parsers in package " + logParserPackage);
+            if (debug) System.out.println("Searching for additional content parsers in package " + logParserPackage);
             // getting an uri to the parser subpackage
             String packageURI = plasmaParser.class.getResource("/"+logParserPackage.replace('.','/')).toString();
-            if (enabled) System.out.println("LogParser directory is " + packageURI);
+            if (debug) System.out.println("LogParser directory is " + packageURI);
             
             File parserDir = new File(new URI(packageURI));
             //System.out.println(parserDir.toString());
@@ -49,11 +93,11 @@ public class LogalizerHandler extends Handler {
                         LogParser theLogParser = (LogParser) theParser;
                         //System.out.println(bla.getName() + " is a logParser");
                         parsers.put(theLogParser.getParserType(), theParser);
-                        if (enabled) System.out.println("Added " + theLogParser.getClass().getName() + " as " + theLogParser.getParserType() + " Parser.");
+                        if (debug) System.out.println("Added " + theLogParser.getClass().getName() + " as " + theLogParser.getParserType() + " Parser.");
                     }
                     else {
                         //System.out.println(bla.getName() + " is not a logParser");
-                        if (enabled) System.out.println("Rejected " + tempClass.getName() + ". Class does not implement the logParser-Interface");
+                        if (debug) System.out.println("Rejected " + tempClass.getName() + ". Class does not implement the logParser-Interface");
 
                     }
                 }
@@ -78,6 +122,7 @@ public class LogalizerHandler extends Handler {
         String className = getClass().getName();
 
         if(manager.getProperty(className + ".enabled").equalsIgnoreCase("true")) enabled = true;
+        if(manager.getProperty(className + ".debug").equalsIgnoreCase("true")) debug = true;
 
         logParserPackage = manager.getProperty(className + ".parserPackage");
 
@@ -89,8 +134,8 @@ public class LogalizerHandler extends Handler {
             LogParser temp = (LogParser) parsers.get(record.getLoggerName());
             if (temp != null) {
                 int returnV = temp.parse(record.getLevel().toString(), record.getMessage());
-                //if (enabled) System.out.println("Logalizertest: " + returnV + " --- " + record.getLevel() + " --- " + record.getMessage());
-                if (enabled) System.out.println("Logalizertest: " + returnV + " --- " + record.getLevel());
+                //if (debug) System.out.println("Logalizertest: " + returnV + " --- " + record.getLevel() + " --- " + record.getMessage());
+                if (debug) System.out.println("Logalizertest: " + returnV + " --- " + record.getLevel());
             }
         }
     }
