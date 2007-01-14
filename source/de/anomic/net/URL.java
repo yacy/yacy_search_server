4b23e79f5128f1461c427509fffff7b8530acc82
karlchenofhell <karlchenofhell@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2007-01-14 17:31:08 +0000

    - quick-fix for http://www.yacy-forum.de/viewtopic.php?t=3358
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@3201 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/net/URL.java
+++ source/de/anomic/net/URL.java
@@ -190,14 +190,14 @@ public class URL {
     /**
      * Escapes the following parts of the url, this object already contains:
      * <ul>
-     * <li>path: see {@link: escape(String)}</li>
+     * <li>path: see {@link: #escape(String)}</li>
      * <li>ref: same as above</li>
      * <li>quest: same as above without the ampersand ("&amp;") and the equals symbol</li>
      * </ul>
      */
     private void escape() {
         if (path != null && path.indexOf('%') == -1) escapePath();
-        if (quest != null && quest.indexOf('%') == -1) escapeQuest();
+        // if (quest != null && quest.indexOf('%') == -1) escapeQuest();
         if (ref != null && ref.indexOf('%') == -1) escapeRef();
     }
     
@@ -230,37 +230,37 @@ public class URL {
     
     final static String[] hex = {
         "%00", "%01", "%02", "%03", "%04", "%05", "%06", "%07",
-        "%08", "%09", "%0a", "%0b", "%0c", "%0d", "%0e", "%0f",
+        "%08", "%09", "%0A", "%0B", "%0C", "%0D", "%0E", "%0F",
         "%10", "%11", "%12", "%13", "%14", "%15", "%16", "%17",
-        "%18", "%19", "%1a", "%1b", "%1c", "%1d", "%1e", "%1f",
+        "%18", "%19", "%1A", "%1B", "%1C", "%1D", "%1E", "%1F",
         "%20", "%21", "%22", "%23", "%24", "%25", "%26", "%27",
-        "%28", "%29", "%2a", "%2b", "%2c", "%2d", "%2e", "%2f",
+        "%28", "%29", "%2A", "%2B", "%2C", "%2D", "%2E", "%2F",
         "%30", "%31", "%32", "%33", "%34", "%35", "%36", "%37",
-        "%38", "%39", "%3a", "%3b", "%3c", "%3d", "%3e", "%3f",
+        "%38", "%39", "%3A", "%3B", "%3C", "%3D", "%3E", "%3F",
         "%40", "%41", "%42", "%43", "%44", "%45", "%46", "%47",
-        "%48", "%49", "%4a", "%4b", "%4c", "%4d", "%4e", "%4f",
+        "%48", "%49", "%4A", "%4B", "%4C", "%4D", "%4E", "%4F",
         "%50", "%51", "%52", "%53", "%54", "%55", "%56", "%57",
-        "%58", "%59", "%5a", "%5b", "%5c", "%5d", "%5e", "%5f",
+        "%58", "%59", "%5A", "%5B", "%5C", "%5D", "%5E", "%5F",
         "%60", "%61", "%62", "%63", "%64", "%65", "%66", "%67",
-        "%68", "%69", "%6a", "%6b", "%6c", "%6d", "%6e", "%6f",
+        "%68", "%69", "%6A", "%6B", "%6C", "%6D", "%6E", "%6F",
         "%70", "%71", "%72", "%73", "%74", "%75", "%76", "%77",
-        "%78", "%79", "%7a", "%7b", "%7c", "%7d", "%7e", "%7f",
+        "%78", "%79", "%7A", "%7B", "%7C", "%7D", "%7E", "%7F",
         "%80", "%81", "%82", "%83", "%84", "%85", "%86", "%87",
-        "%88", "%89", "%8a", "%8b", "%8c", "%8d", "%8e", "%8f",
+        "%88", "%89", "%8A", "%8B", "%8C", "%8D", "%8E", "%8F",
         "%90", "%91", "%92", "%93", "%94", "%95", "%96", "%97",
-        "%98", "%99", "%9a", "%9b", "%9c", "%9d", "%9e", "%9f",
-        "%a0", "%a1", "%a2", "%a3", "%a4", "%a5", "%a6", "%a7",
-        "%a8", "%a9", "%aa", "%ab", "%ac", "%ad", "%ae", "%af",
-        "%b0", "%b1", "%b2", "%b3", "%b4", "%b5", "%b6", "%b7",
-        "%b8", "%b9", "%ba", "%bb", "%bc", "%bd", "%be", "%bf",
-        "%c0", "%c1", "%c2", "%c3", "%c4", "%c5", "%c6", "%c7",
-        "%c8", "%c9", "%ca", "%cb", "%cc", "%cd", "%ce", "%cf",
-        "%d0", "%d1", "%d2", "%d3", "%d4", "%d5", "%d6", "%d7",
-        "%d8", "%d9", "%da", "%db", "%dc", "%dd", "%de", "%df",
-        "%e0", "%e1", "%e2", "%e3", "%e4", "%e5", "%e6", "%e7",
-        "%e8", "%e9", "%ea", "%eb", "%ec", "%ed", "%ee", "%ef",
-        "%f0", "%f1", "%f2", "%f3", "%f4", "%f5", "%f6", "%f7",
-        "%f8", "%f9", "%fa", "%fb", "%fc", "%fd", "%fe", "%ff"
+        "%98", "%99", "%9A", "%9B", "%9C", "%9D", "%9E", "%9F",
+        "%A0", "%A1", "%A2", "%A3", "%A4", "%A5", "%A6", "%A7",
+        "%A8", "%A9", "%AA", "%AB", "%AC", "%AD", "%AE", "%AF",
+        "%B0", "%B1", "%B2", "%B3", "%B4", "%B5", "%B6", "%B7",
+        "%B8", "%B9", "%BA", "%BB", "%BC", "%BD", "%BE", "%BF",
+        "%C0", "%C1", "%C2", "%C3", "%C4", "%C5", "%C6", "%C7",
+        "%C8", "%C9", "%CA", "%CB", "%CC", "%CD", "%CE", "%CF",
+        "%D0", "%D1", "%D2", "%D3", "%D4", "%D5", "%D6", "%D7",
+        "%D8", "%D9", "%DA", "%DB", "%DC", "%DD", "%DE", "%DF",
+        "%E0", "%E1", "%E2", "%E3", "%E4", "%E5", "%E6", "%E7",
+        "%E8", "%E9", "%EA", "%EB", "%EC", "%ED", "%EE", "%EF",
+        "%F0", "%F1", "%F2", "%F3", "%F4", "%F5", "%F6", "%F7",
+        "%F8", "%F9", "%FA", "%FB", "%FC", "%FD", "%FE", "%FF"
     };
     
     /**
@@ -305,7 +305,7 @@ public class URL {
                     || ch == '.' || ch == '!'
                     || ch == '~' || ch == '*'
                     || ch == '\'' || ch == '('
-                    || ch == ')') {
+                    || ch == ')' || ch == ';') {
                 sbuf.append((char)ch);
             } else if (ch <= 0x007f) {              // other ASCII
                 sbuf.append(hex[ch]);
@@ -525,8 +525,9 @@ public class URL {
           new String[]{null, "ftp://ftp.delegate.org/"},
           new String[]{"http://www.anomic.de/home", "ftp://ftp.delegate.org/"},
           new String[]{"http://www.anomic.de","mailto:yacy@weltherrschaft.org"},
-          new String[]{"http://www.anomic.de","javascipt:temp"}
-        };
+          new String[]{"http://www.anomic.de","javascipt:temp"},
+          new String[]{null, "http://diskusjion.no/index.php?s=5bad5f431a106d9a8355429b81bb0ca5&amp;showuser=23585"}
+          };
         String environment, url;
         de.anomic.net.URL aURL = null;
         java.net.URL jURL = null;
