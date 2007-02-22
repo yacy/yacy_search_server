a5a36d92524227b20c682a99e93bdaf561890271
karlchenofhell <karlchenofhell@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2007-02-22 08:04:09 +0000

    - hopefully last fix fo 1.5 methods (sorry for that, eclipse isn't that helpful in identifying those methods)
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@3387 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/data/wiki/tokens/SimpleToken.java
+++ source/de/anomic/data/wiki/tokens/SimpleToken.java
@@ -1,9 +1,53 @@
+// SimpleToken.java 
+// ---------
+// part of YaCy
+// (C) by Michael Peter Christen; mc@anomic.de
+// first published on http://www.anomic.de
+// Frankfurt, Germany, 2007
+// Created 22.02.2007
+//
+// This file is contributed by Franz Brauße
+//
+// $LastChangedDate: $
+// $LastChangedRevision: $
+// $LastChangedBy: $
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// Using this software in any meaning (reading, learning, copying, compiling,
+// running) means that you agree that the Author(s) is (are) not responsible
+// for cost, loss of data or any harm that may be caused directly or indirectly
+// by usage of this softare or this documentation. The usage of this software
+// is on your own risk. The installation and usage (starting/running) of this
+// software may allow other people or application to access your computer and
+// any attached devices and is highly dependent on the configuration of the
+// software which must be done by the user of the software; the author(s) is
+// (are) also not responsible for proper configuration and usage of the
+// software, even if provoked by documentation provided together with
+// the software.
+//
+// Any changes to this file according to the GPL as documented in the file
+// gpl.txt aside this file in the shipment you received can be done to the
+// lines that follows this copyright notice here, but changes must not be
+// done inside the copyright notive above. A re-distribution must contain
+// the intact and unchanged copyright notice.
+// Contributions and changes to the program code must be marked as such.
 
 package de.anomic.data.wiki.tokens;
 
-
 import java.util.ArrayList;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,7 +57,6 @@ public class SimpleToken extends AbstractToken {
 	protected int grade = 0;
 	
 	protected final Pattern[] pattern;
-	protected MatchResult mresult = null;
 	private final String[][] definitionList;
 	private final String[] blockElements;
 	
@@ -37,16 +80,10 @@ public class SimpleToken extends AbstractToken {
 				i++;
 				break;
 			}
-		this.pattern = new Pattern[] { Pattern.compile(String.format(
-				"([\\%s]{%d,%d})(.*?)([\\%s]{%d,%d})",
-				new Object[] {
-                        new Character(firstChar),
-                        new Integer(i),
-                        new Integer(definitionList.length),
-                        new Character(lastChar),
-                        new Integer(i),
-                        new Integer(definitionList.length)
-                } ))};
+		this.pattern = new Pattern[] { Pattern.compile(
+				"([\\" + firstChar + "]{" + i + "," + definitionList.length + "})" +
+                "(.*?)" +
+                "([\\" + lastChar + "]{" + i + "," + definitionList.length + "})")};
 	}
 	
 	public String getMarkup() {
