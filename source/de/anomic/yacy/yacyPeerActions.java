5941577076d541bdb70696d8fb2f4818b4b33f91
auron_x <auron_x@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2007-03-25 14:56:42 +0000

    *) added some logging to PPM-Calculation to find a rare bug
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@3521 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/yacy/yacyPeerActions.java
+++ source/de/anomic/yacy/yacyPeerActions.java
@@ -119,6 +119,11 @@ public class yacyPeerActions {
         
         //the speed of indexing (pages/minute) of the peer
         sb.totalPPM = (int) (sb.indexedPages * 60 / Math.max(uptime, 1));
+        yacyCore.log.logInfo("PPM-Calculation\n\t"+
+        		Math.max((float) indexedcdiff, 0f)+" Pages * 60 / "+
+        		Math.max((float) uptimediff, 1f)+" Seconds = "+
+        		(Math.max((float) indexedcdiff, 0f) * 60f / Math.max((float) uptimediff, 1f))+" PPM"
+        	);
         seedDB.mySeed.put(yacySeed.ISPEED, Long.toString(Math.round(Math.max((float) indexedcdiff, 0f) * 60f / Math.max((float) uptimediff, 1f))));
         sb.totalQPM = sb.requestedQueries * 60d / Math.max((double) uptime, 1d);
         seedDB.mySeed.put(yacySeed.RSPEED, Double.toString(sb.totalQPM /*Math.max((float) requestcdiff, 0f) * 60f / Math.max((float) uptimediff, 1f)*/ ));
