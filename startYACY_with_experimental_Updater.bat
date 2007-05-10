d5e634d06bd0cc6e02d68f52e6e735b658a4cd6c
rramthun <rramthun@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2007-05-10 20:47:23 +0000

    *) Updater r3 - Please test and report major problems (don't use this version for stable-release, r3 updates always to latest dev-version of YaCy)
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@3703 6c8d7289-2bf4-0310-a012-ef5d649a1542

---
+++ startYACY_with_experimental_Updater.bat
@@ -0,0 +1,67 @@
+@Echo Off
+title YaCy
+
+If %1.==CPGEN. GoTo :CPGEN
+
+Rem Generating the proper classpath unsing loops and labels
+Set CLASSPATH=classes;htroot;lib/updater/YaCyUpdater_r3.jar
+For %%X in (lib/*.jar) Do Call %0 CPGEN lib\%%X
+For %%X in (libx/*.jar) Do Call %0 CPGEN libx\%%X
+
+REM Please change the "javastart" settings in the web-interface "Basic Configuration" -> "Advanced" 
+set jmx=
+set jms=
+set javacmd=-Xmx64m -Xms10m
+set priolvl=0
+set priority=/NORMAL
+if exist DATA\SETTINGS\httpProxy.conf GoTo :GETSTARTOPTS
+
+:STARTJAVA
+Rem Starting YaCy
+Echo Generated classpath:%CLASSPATH%
+Echo JRE Parameters:%javacmd%
+Echo Priority:%priority%
+
+Echo ****************** YaCy Web Crawler/Indexer ^& Search Engine *******************
+Echo **** (C) by Michael Peter Christen, usage granted under the GPL Version 2  ****
+Echo **** USE AT YOUR OWN RISK! Project home and releases: http://yacy.net/yacy ****
+Echo **  LOG of       YaCy: DATA/LOG/yacy00.log (and yacy^<xx^>.log)                **
+Echo **  STOP         YaCy: execute stopYACY.bat and wait some seconds            **
+Echo **  GET HELP for YaCy: see www.yacy-websearch.net/wiki and www.yacy-forum.de **
+Echo *******************************************************************************
+Echo  ^>^> YaCy started as daemon process. Administration at http://localhost:%port% ^<^<
+
+start "YaCy" %priority% /B /WAIT java %javacmd% -classpath %CLASSPATH% YaCyUpdater
+
+if not exist DATA\yacy.restart GoTo :END
+:WAIT
+echo Please wait...
+REM 'sleep' five seconds
+ping localhost -n 5 >NUL
+if exist DATA\yacy.running GoTo :WAIT
+del DATA\yacy.restart
+
+Rem This target is used to read java runtime parameters out of the yacy config file
+:GETSTARTOPTS
+for /F "tokens=1,2 delims==" %%i in (DATA\SETTINGS\httpProxy.conf) do (
+    if "%%i"=="javastart_Xmx" set jmx=%%j
+    if "%%i"=="javastart_Xms" set jms=%%j
+    if "%%i"=="port" set port=%%j
+    if "%%i"=="javastart_priority" set priolvl=%%j
+)
+if defined jmx set javacmd=-%jmx%
+if defined jms set javacmd=-%jms% %javacmd%
+if not defined port set port=8080
+if defined priolvl (
+    if %priolvl% == 20 set priority=/LOW
+    if %priolvl% == 10 set priority=/BELOWNORMAL
+)
+
+GoTo :STARTJAVA
+
+Rem This target is used to concatenate the classpath parts 
+:CPGEN
+Set CLASSPATH=%CLASSPATH%;%2
+
+Rem Target needed to jump to the end of the file
+:END
---
+++ startYACY_with_experimental_Updater.command
@@ -0,0 +1,11 @@
+cd `dirname $0`
+./startYACY_with_experimental_Updater.sh &
+echo "****************** YaCy Web Crawler/Indexer & Search Engine *******************"
+echo "**** (C) by Michael Peter Christen, usage granted under the GPL Version 2  ****"
+echo "**** USE AT YOUR OWN RISK! Project home and releases: http://yacy.net/yacy ****"
+echo "**  LOG of       YaCy: DATA/LOG/yacy00.log (and yacy<xx>.log)                **"
+echo "**  STOP         YaCy: execute stopYACY.sh and wait some seconds             **"
+echo "**  GET HELP for YaCy: see www.yacy-websearch.net/wiki and www.yacy-forum.de **"
+echo "*******************************************************************************"
+echo " >> YaCy started as daemon process. Administration at http://localhost:8080 <<"
+echo "    You can close this window now, this will NOT shut down your YaCy peer."
