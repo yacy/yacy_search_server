54ddb3262ca5f83ced4510c80c33563c16a94bcc
allo <allo@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2007-06-02 14:50:36 +0000

    enter on webstrucutre
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@3783 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/js/WatchWebStructure.js
+++ htroot/js/WatchWebStructure.js
@@ -1,3 +1,8 @@
 function changeHost(){
 	window.location.replace("http://"+window.location.host+":"+window.location.port+"/WatchWebStructure_p.html?host="+document.getElementById("host").value);
 }
+function keydown(ev){
+        if(ev.which==13){
+                changeHost();
+        }
+}
