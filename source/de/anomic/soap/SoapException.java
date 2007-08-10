4798044708f43986a2105f2ba2ad8431de07327d
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2007-08-10 14:03:07 +0000

    fixed compile problem with svn 4037
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@4038 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/soap/SoapException.java
+++ source/de/anomic/soap/SoapException.java
@@ -52,7 +52,7 @@ import org.apache.axis.Constants;
 import org.apache.axis.Message;
 import org.apache.axis.MessageContext;
 import org.apache.axis.message.SOAPEnvelope;
-import org.apache.axis.message.SOAPFault;
+import org.apache.axis.message.SOAPFault;   
 
 import de.anomic.http.httpHeader;
 
