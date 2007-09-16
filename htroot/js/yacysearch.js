1880bba420af84cf3ff1e7f54663a9475de6b9bb
fuchsi <fuchsi@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2007-09-16 14:30:53 +0000

    A few changes to the progress bar and search result statistics layout influenced by the discussion in <http://forum.yacy-websuche.de/viewtopic.php?f=5&t=268> with the idea of saving vertical space. Please check in every available browser and comment wether it's better than before. ;)
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@4100 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/js/yacysearch.js
+++ htroot/js/yacysearch.js
@@ -12,23 +12,22 @@ function Progressbar(length, parent) {
 
   // use this function to display the progress, because it updates everything
   this.step = function(count) {
-    this.position += count;
+    if (this.position < this.length)
+      this.position += count;
+
     // update the bar
     this.percentage = this.position*(100/this.length);
     this.fill.style.width = this.percentage + "%";
 
     // if the end is reached, the bar is hidden/removed
-    if(this.position==this.length)
-      removeAllChildren(this.element);
+    if(this.position >= this.length) {
+      this.fill.style.visibility = "hidden";
+    }
   }
 
   // the actual progressbar
   var bar = document.createElement("div");
   bar.className = "ProgressBar";
-  bar.style.width = "100%";
-  bar.style.height = "20px";
-  bar.style.margin = "10px auto";
-  bar.style.textAlign = "left";
 
   // the actual bar
   this.fill = document.createElement("div");
@@ -36,17 +35,21 @@ function Progressbar(length, parent) {
   this.fill.style.width = "0%"
   bar.appendChild(this.fill);
 
-  // a description for the bar
-  var description = document.createTextNode(DESCRIPTION_STRING);
-  var textcontainer = document.createElement("strong");
-  textcontainer.appendChild(description);
-
   // the container for the elements used by the Progressbar
   this.element = document.createElement("div");
   this.element.style.textAlign = "center";
   // get hasLayout in IE, needed because of the percentage as width of the bar
   this.element.className = "gainlayout";
-  this.element.appendChild(textcontainer);
+
+  // results counter inside progress bar
+  var resCounter = document.getElementById("resCounter");
+  resCounter.style.display = "inline";
+  bar.appendChild(resCounter);
+
+  // the result sites navigation
+  var pagenav = document.getElementById("pagenav");
+  pagenav.style.display = "inline";
+  bar.appendChild(pagenav);
   this.element.appendChild(bar);
   parent.appendChild(this.element);
 }
