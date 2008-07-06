f0012554de0b7849235ef822fbebda0e776c5453
f1ori <f1ori@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2008-07-06 16:48:41 +0000

    * improved localsearch.sh:   * read port from DATA/SETTINGS/yacy.conf   * use curl or wget, depending on what is available
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@4972 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- bin/localsearch.sh
+++ bin/localsearch.sh
@@ -1,1 +1,10 @@
-curl -s "http://localhost:8080/yacysearch.rss?query=$1&resource=local&verify=false" | grep link | grep -v opensearchdescription | grep -v yacysearch | grep -v 'yacy:item' | sed 's/<link>//' | sed 's/<\/link>//'
+port=$(grep ^port= ../DATA/SETTINGS/yacy.conf |cut -d= -f2)
+if which curl &>/dev/null; then
+  httpfetcher="curl -s"
+elif which wget &>/dev/null; then
+  httpfetcher="wget -q -O - "
+else
+  echo "Neither curl nor wget installed!"
+  exit 1
+fi
+eval $httpfetcher "http://localhost:$port/yacysearch.rss?query=$1&resource=local&verify=false" | grep link | grep -v opensearchdescription | grep -v yacysearch | grep -v 'yacy:item' | sed 's/<link>//' | sed 's/<\/link>//'
