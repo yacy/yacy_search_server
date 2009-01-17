98ab7ae20aac285c11e3613f7c946b6c5ba31946
apfelmaennchen <apfelmaennchen@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2009-01-17 09:51:04 +0000

    small fix
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@5501 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/yacysearchitem.java
+++ htroot/yacysearchitem.java
@@ -138,7 +138,7 @@ public class yacysearchitem {
                     (((wordURL = yacyURL.probablyWordURL(result.hash(), query[0])) != null) ? ", probablyWordURL=" + wordURL.toNormalform(false, true) : ""));
             final plasmaSnippetCache.TextSnippet snippet = result.textSnippet();
             prop.put("content_description", (snippet == null) ? "" : snippet.getLineMarked(theQuery.fullqueryHashes));
-            prop.putXML("content_description", (snippet == null) ? "" : snippet.getLineMarked(theQuery.fullqueryHashes));
+            prop.putXML("content_description-xml", (snippet == null) ? "" : snippet.getLineMarked(theQuery.fullqueryHashes));
             serverProfiling.update("SEARCH", new plasmaProfiling.searchEvent(theQuery.id(true), plasmaSearchEvent.FINALIZATION + "-" + item, 0, 0));
             
             return prop;
