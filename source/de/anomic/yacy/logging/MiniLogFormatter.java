78b73619378e0a8d98daa829f60a399e674d628a
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2009-02-01 00:21:08 +0000

    fixed problem with logging YOU MUST DELETE DATA/LOG TO MAKE THIS WORK! (sorry..)
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@5552 6c8d7289-2bf4-0310-a012-ef5d649a1542

---
+++ source/de/anomic/yacy/logging/MiniLogFormatter.java
@@ -0,0 +1,52 @@
+//severMiniLogFormatter.java 
+//-------------------------------------
+//part of YACY
+//(C) by Michael Peter Christen; mc@yacy.net
+//first published on http://www.anomic.de
+//Frankfurt, Germany, 2004
+//
+//This file ist contributed by Martin Thelian
+//last major change: $LastChangedDate: 2008-12-04 13:54:16 +0100 (Thu, 04 Dec 2008) $ by $LastChangedBy: orbiter $
+//Revision: $LastChangedRevision: 5379 $
+//
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package de.anomic.yacy.logging;
+
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+public final class MiniLogFormatter extends SimpleFormatter {
+
+      private final StringBuilder buffer = new StringBuilder();
+  
+      public MiniLogFormatter() {
+          super();
+      }        
+      
+      public synchronized String format(final LogRecord record) {
+          
+          final StringBuilder buffer = this.buffer;
+          buffer.setLength(0);
+
+          buffer.append(formatMessage(record));
+          
+          // adding the stack trace if available
+          buffer.append(System.getProperty("line.separator"));
+          
+          
+          return buffer.toString();
+      }
+}
