3faa011e3d3bba55279dad7f7b5df9e3de742598
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2009-09-25 14:03:41 +0000

    added another search integration help page
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@6346 6c8d7289-2bf4-0310-a012-ef5d649a1542

---
+++ htroot/ConfigSearchBox.java
@@ -0,0 +1,39 @@
+// ConfigSearchBox.java 
+// -----------------------
+// (C) 2009 by Michael Peter Christen; mc@yacy.net
+// first published 25.09.2009 on http://yacy.net
+//
+// LICENSE
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import de.anomic.http.metadata.RequestHeader;
+import de.anomic.search.Switchboard;
+import de.anomic.server.serverObjects;
+import de.anomic.server.serverSwitch;
+
+public class ConfigSearchBox {
+
+    public static serverObjects respond(final RequestHeader header, final serverObjects post, final serverSwitch env) {
+        final serverObjects prop = new serverObjects();
+        final Switchboard sb = (Switchboard) env;
+        
+        String myaddress = sb.peers.mySeed().getPublicAddress();
+        if (myaddress == null) myaddress = "localhost:" + sb.getConfig("port", "8080");
+        prop.put("myaddress", myaddress);
+        return prop;
+    }
+
+}
