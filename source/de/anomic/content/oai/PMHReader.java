5bd1c1d205913d771da69e1ea3563b605a4e075b
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2009-10-01 22:56:22 +0000

    just added some comments that had been produced to learn about OAI-PMH
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@6369 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/de/anomic/content/oai/PMHReader.java
+++ source/de/anomic/content/oai/PMHReader.java
@@ -86,3 +86,149 @@ public class PMHReader {
     }
     
 }
+/*
+
+http://an.oa.org/OAI-script?verb=GetRecord&identifier=oai:arXiv.org:hep-th/9901001&metadataPrefix=oai_dc
+
+special characters in URIs must be encoded, the correct form of the above GET request URL is:
+
+http://an.oa.org/OAI-script?verb=GetRecord&identifier=oai%3AarXiv.org%3Ahep-th%2F9901001&metadataPrefix=oai_dc
+
+"/","%2F"
+"?","%3F"
+"#","%23"
+"=","%3D"
+"&","%26"
+":","%3A"
+";","%3B"
+" ","%20"
+"%","%25"
+"+","%2B"
+
+GetRecord
+http://arXiv.org/oai2?verb=GetRecord&identifier=oai:arXiv.org:cs/0112017&metadataPrefix=oai_dc
+http://opus.bsz-bw.de/fhhv/oai2/oai2.php?verb=GetRecord&identifier=oai:opus.bsz-bw.de-fhhv:6&metadataPrefix=oai_dc
+
+Identify
+http://memory.loc.gov/cgi-bin/oai?verb=Identify
+
+ListIdentifiers
+http://an.oa.org/OAI-script?verb=ListIdentifiers&from=1998-01-15&metadataPrefix=oldArXiv&set=physics:hep
+http://an.oa.org/OAI-script?verb=ListIdentifiers&resumptionToken=xxx45abttyz
+http://www.perseus.tufts.edu/cgi-bin/pdataprov?verb=ListIdentifiers&metadataPrefix=olac&from=2001-01-01&until=2001-01-01&set=Perseus:collection:PersInfo
+
+ListMetadataFormats
+http://www.perseus.tufts.edu/cgi-bin/pdataprov?verb=ListMetadataFormats&identifier=oai:perseus.tufts.edu:Perseus:text:1999.02.0119
+http://memory.loc.gov/cgi-bin/oai?verb=ListMetadataFormats
+http://memory.loc.gov/cgi-bin/oai?verb=ListMetadataFormats&identifier=oai:lcoa1.loc.gov:loc.rbc/rbpe.00000111
+http://opus.bsz-bw.de/fhhv/oai2/oai2.php?verb=ListMetadataFormats
+
+ListRecords
+http://opus.bsz-bw.de/fhhv/oai2/oai2.php?verb=ListRecords&metadataPrefix=oai_dc
+http://opus.bsz-bw.de/fhhv/oai2/oai2.php?verb=ListRecords&resumptionToken=455
+http://opus.bsz-bw.de/fhhv/oai2/oai2.php?verb=ListRecords&resumptionToken=890
+http://an.oa.org/OAI-script?verb=ListRecords&from=1998-01-15&set=physics:hep&metadataPrefix=oai_rfc1807
+http://www.perseus.tufts.edu/cgi-b:in/pdataprov?verb=ListRecords&from=2002-05-01T14:15:00Z&until=2002-05-01T14:20:00Z&metadataPrefix=oai_dc
+http://memory.loc.gov/cgi-bin/oai?verb=ListRecords&from=2002-06-01T02:00:00Z&until=2002-06-01T03:00:00Z&metadataPrefix=oai_marc
+
+ListSets
+http://an.oa.org/OAI-script?verb=ListSets
+http://purl.org/alcme/etdcat/servlet/OAIHandler?verb=ListSets
+
+urn identifier können über den resolver der d-nb aufgelöst werden:
+http://nbn-resolving.de/urn:nbn:de:bsz:960-opus-1860
+
+<?xml version="1.0" encoding="UTF-8"?>
+<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
+         http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd">
+ <responseDate>2009-10-01T22:20:04Z</responseDate>
+ <request verb="ListRecords" metadataPrefix="oai_dc">http://opus.bsz-bw.de/fhhv/oai2/oai2.php</request>
+ <ListRecords>
+  <record>
+   <header>
+    <identifier>oai:opus.bsz-bw.de-fhhv:1</identifier>
+    <datestamp>2008-03-04T12:17:33Z</datestamp>
+    <setSpec>ddc:020</setSpec>
+    <setSpec>pub-type:2</setSpec>
+    <setSpec>has-source-swb:false</setSpec>
+   </header>
+   <metadata>
+     <oai_dc:dc
+       xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+       xmlns:dc="http://purl.org/dc/elements/1.1/"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/
+       http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+      <dc:title>Teaching Information Literacy with the Lerninformationssystem</dc:title>
+      <dc:creator>Hauschke, Christian</dc:creator>
+      <dc:creator>Ullmann, Nadine</dc:creator>
+      <dc:subject>Informationskompetenz</dc:subject>
+      <dc:subject>E-Learning</dc:subject>
+      <dc:subject>Bibliothek</dc:subject>
+      <dc:subject>Informationsvermittlung</dc:subject>
+      <dc:subject>Wissenschaftliches Arbeiten</dc:subject>
+      <dc:subject>information literacy</dc:subject>
+      <dc:subject>e-learning</dc:subject>
+      <dc:subject>library</dc:subject>
+      <dc:subject>information dissemination</dc:subject>
+      <dc:subject>Library and information sciences</dc:subject>
+      <dc:description>A German university has developed a learning information system to improve information literacy among German students. An online tutorial based on this Lerninformationssystem has been developed. The structure of this learning information system is described, an online tutorial based on it is illustrated, and the different learning styles that it supports are indicated.</dc:description>
+      <dc:publisher>Fachhochschule Hannover</dc:publisher>
+      <dc:publisher>Sonstige Einrichtungen. Sonstige Einrichtungen</dc:publisher>
+      <dc:date>2006</dc:date>
+      <dc:type>Article</dc:type>
+      <dc:format>application/pdf</dc:format>
+      <dc:identifier>urn:nbn:de:bsz:960-opus-10</dc:identifier>
+      <dc:identifier>http://opus.bsz-bw.de/fhhv/volltexte/2008/1/</dc:identifier>
+      <dc:source>Australian Academic &amp; Research Libraries, 37 (1), S. 55-60</dc:source>
+      <dc:language>eng</dc:language>
+      <dc:rights>http://creativecommons.org/licenses/by/2.0/de/deed.de</dc:rights>
+     </oai_dc:dc>
+   </metadata>
+  </record>
+  <record>
+   <header>
+    <identifier>oai:opus.bsz-bw.de-fhhv:52</identifier>
+    <datestamp>2008-10-30T11:24:12Z</datestamp>
+    <setSpec>ddc:360</setSpec>
+    <setSpec>pub-type:20</setSpec>
+    <setSpec>has-source-swb:false</setSpec>
+   </header>
+   <metadata>
+     <oai_dc:dc
+       xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+       xmlns:dc="http://purl.org/dc/elements/1.1/"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/
+       http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+      <dc:title>Akademisierung von Pflege</dc:title>
+      <dc:creator>Oelke, Uta-Karola</dc:creator>
+      <dc:subject>Pflege</dc:subject>
+      <dc:subject>Pflegewissenschaft</dc:subject>
+      <dc:subject>Akademisierung</dc:subject>
+      <dc:subject>Ausbildung</dc:subject>
+      <dc:subject>&quot;Social services; association&quot;</dc:subject>
+      <dc:description>Die Arbeit bietet einen Überblick über pflegebezogene Studiengänge und diskutiert die Akademisierung von Pflege.</dc:description>
+      <dc:publisher>Fachhochschule Hannover</dc:publisher>
+      <dc:publisher>Fakultät V - Diakonie, Gesundheit und Soziales. Fakultät V - Diakonie, Gesundheit und Soziales</dc:publisher>
+      <dc:contributor>Werner-Schule vom DRK</dc:contributor>
+      <dc:date>1994</dc:date>
+      <dc:type>TechReport</dc:type>
+      <dc:format>application/pdf</dc:format>
+      <dc:identifier>urn:nbn:de:bsz:960-opus-520</dc:identifier>
+      <dc:identifier>http://opus.bsz-bw.de/fhhv/volltexte/2008/52/</dc:identifier>
+      <dc:language>ger</dc:language>
+      <dc:rights>/fhhv/doku/urheberrecht.php?la=de</dc:rights>
+     </oai_dc:dc>
+   </metadata>
+  </record>
+  <resumptionToken
+     expirationDate="2009-10-02T20:20:04Z"
+     completeListSize="226"
+     cursor="0">119</resumptionToken>
+ </ListRecords>
+</OAI-PMH>
+
+*/
