77d6604856f645275ed3a90b52c972891f25766d
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2009-10-12 17:41:16 +0000

    fix for npe, see http://forum.yacy-websuche.de/viewtopic.php?p=17727#p17727
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@6403 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/net/yacy/kelondro/order/MergeIterator.java
+++ source/net/yacy/kelondro/order/MergeIterator.java
@@ -149,7 +149,7 @@ public class MergeIterator<E> implements CloneableIterator<E> {
     static {
         Method meth = null;
         try {
-            final Class<?> c = Class.forName("de.anomic.kelondro.order.MergeIterator");
+            final Class<?> c = Class.forName("net.yacy.kelondro.order.MergeIterator");
             meth = c.getMethod("mergeEqualByReplace", new Class[]{Object.class, Object.class});
         } catch (final SecurityException e) {
             System.out.println("Error while initializing simpleMerge (1): " + e.getMessage());
