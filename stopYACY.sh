35d0057cb0b3603167d6b4e6acf31fd68c1a9b48
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2010-03-11 00:12:53 +0000

    stopYACY.sh can now use curl
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@6741 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- stopYACY.sh
+++ stopYACY.sh
@@ -5,7 +5,14 @@ if [ -x `which wget` ]
 then
 	bin/apicall.sh "Steering.html?shutdown=true"
 
-echo "Please wait until the YaCy daemon process terminates"
+echo "Please wait until the YaCy daemon process terminates [wget]"
+echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
+
+elif [ -x `which curl` ]
+then
+	bin/apicall.sh "Steering.html?shutdown=true"
+
+echo "Please wait until the YaCy daemon process terminates [curl]"
 echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
 
 elif [ -x `which java` ]
@@ -17,7 +24,7 @@ then
 
 	java -classpath classes:htroot:$CLASSPATH net.yacy.yacy -shutdown
 
-	echo "Please wait until the YaCy daemon process terminates"
+	echo "Please wait until the YaCy daemon process terminates [java]"
 	echo "You can monitor this with 'tail -f DATA/LOG/yacy00.log' and 'fuser log/yacy00.log'"
 
 else
