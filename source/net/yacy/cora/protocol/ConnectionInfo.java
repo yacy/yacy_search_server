670c746dc560c6c47a2694079931ac5add84c853
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2010-08-07 23:03:10 +0000

    dual-licensed HttpConnectionInfo for LGPL original GPL license holder granted dual-licensing by email
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@7024 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/net/yacy/cora/protocol/ConnectionInfo.java
+++ source/net/yacy/cora/protocol/ConnectionInfo.java
@@ -1,28 +1,27 @@
-// HttpConnectionInfo.java
-// (C) 2008 by Daniel Raap; danielr@users.berlios.de
-// first published 07.04.2008 on http://yacy.net
-//
-// This is a part of YaCy, a peer-to-peer based web search engine
-//
-// $LastChangedDate: 2008-03-14 01:16:04 +0100 (Fr, 14 Mrz 2008) $
-// $LastChangedRevision: 4558 $
-// $LastChangedBy: orbiter $
-//
-// LICENSE
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+/**
+ *  HttpConnectionInfo.java
+ *  First published 07.04.2008 by Daniel Raap; danielr@users.berlios.de under the GPL
+ *  Copyright 2010 by Michael Peter Christen for LGPL
+ *  Dual-Licensing for LGPL granted by Daniel Raap 07.08.2010 by email
+ *  
+ *  $LastChangedDate: 2008-03-14 01:16:04 +0100 (Fr, 14 Mrz 2008) $
+ *  $LastChangedRevision: 4558 $
+ *  $LastChangedBy: orbiter $
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package net.yacy.cora.protocol;
 
