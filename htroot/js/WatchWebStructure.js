190de644ddbd387e060d160d5e81e9c7d7fbec72
mikeworks <mikeworks@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2010-09-26 22:28:32 +0000

    de.lng: Added German translations for some missing table content on Network view WatchWebStructure_p.html: Added JS verification of RGB color codes (currently only RGB value is checked but this could be enhanced to also check for websafe colors)
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@7195 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/js/WatchWebStructure.js
+++ htroot/js/WatchWebStructure.js
@@ -6,3 +6,38 @@ function keydown(ev){
                 changeHost();
         }
 }
+
+/* Check if the input matches some RGB hex values */
+function isValidColor(hexcolor) {
+	var strPattern = /^[0-9a-f]{3,6}$/i;
+	return strPattern.test(hexcolor);
+} 
+	
+function checkform(form) {
+	if (isValidColor(form.colorback.value)) {
+		if (isValidColor(form.colortext.value)) {
+			if (isValidColor(form.colorline.value)) {
+				if (isValidColor(form.colordot.value)) {
+					if (isValidColor(form.colorlineend.value)) {
+						return true;
+					} else {
+						alert("Invalid Dot-end value: " + form.colorlineend.value);
+						return false;
+					}
+				} else {
+					alert("Invalid Dot value: " + form.colordot.value);
+					return false;
+				}
+			} else {
+				alert("Invalid Line value: " + form.colorline.value);
+				return false;
+			}
+		} else {
+			alert("Invalid Text value: " + form.colortext.value);
+			return false;
+		}
+	} else {
+		alert("Invalid Background value: " + form.colorback.value);
+		return false;
+	}
+}
