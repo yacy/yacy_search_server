0da3b6489e704d97bde482ef922e033bf34970f5
low012 <low012@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2011-03-07 21:44:58 +0000

    *) added the only changes from r7557 which actualy made sense *) caught potential exception (occured when user entered a string which did not contain digits only for the maximum number of lines) *) use prop.putHTML to avoid potential XSS attack in case an attacker manages to cause something to end up in the logs which contains a string which was defined by the attacker
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@7562 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- htroot/ViewLog_p.java
+++ htroot/ViewLog_p.java
@@ -1,4 +1,4 @@
-//ViewLog_p.java 
+//ViewLog_p.java
 //-----------------------
 //part of the AnomicHTTPD caching proxy
 //(C) by Michael Peter Christen; mc@yacy.net
@@ -6,7 +6,10 @@
 //Frankfurt, Germany, 2004
 //
 //This File is contributed by Alexander Schier
-//last major change: 14.12.2004
+//
+// $LastChangedDate$
+// $LastChangedRevision$
+// $LastChangedBy$
 //
 //This program is free software; you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
@@ -49,20 +52,27 @@ public class ViewLog_p {
         boolean reversed = false;
         boolean json = false;
         int maxlines = 400, lines = 200;
+        /* Usually a regex like this would make no sense, ".*" would be
+         * sufficient, but ".*.*" makes it a little bit more convenient
+         * for the user to input regexes like ".*FOO.*" in the HTML
+         * interface.
+         */
         String filter = ".*.*";
         
         if(post != null){
-            if(post.containsKey("mode") && (post.get("mode")).equals("reversed")){
-                reversed=true;
-            }
+            reversed = (post.containsKey("mode") && "reversed".equals(post.get("mode")));
+            json = post.containsKey("json");
+
             if(post.containsKey("lines")){
-                lines = Integer.parseInt(post.get("lines"));
+                try {
+                    lines = Integer.parseInt(post.get("lines"));
+                } catch (NumberFormatException e) {
+                    Log.logException(e);
+                }
             }
+
             if(post.containsKey("filter")){
                 filter = post.get("filter");
-            }
-            if(post.containsKey("json")){
-                json = true;
             }
         }
         
@@ -70,14 +80,14 @@ public class ViewLog_p {
         final Logger logger = Logger.getLogger("");
         final Handler[] handlers = logger.getHandlers();
         boolean displaySubmenu = false;
-        for (int i=0; i<handlers.length; i++) {
-            if (handlers[i] instanceof GuiHandler) {
-                maxlines = ((GuiHandler)handlers[i]).getSize();
+        for (final Handler handler : handlers) {
+            if (handler instanceof GuiHandler) {
+                maxlines = ((GuiHandler)handler).getSize();
                 if (lines > maxlines) lines = maxlines;
-                log = ((GuiHandler)handlers[i]).getLogLines(reversed,lines);
-            } else if (handlers[i] instanceof LogalizerHandler) {
+                log = ((GuiHandler)handler).getLogLines(reversed,lines);
+            } else if (handler instanceof LogalizerHandler) {
                 displaySubmenu = true;
-            }
+             }
         }
         
         prop.put("submenu", displaySubmenu ? "1" : "0");
@@ -98,22 +108,34 @@ public class ViewLog_p {
 
         int level = 0;
         int lc = 0;
-        for (int i=0; i < log.length; i++) {
-            final String nextLogLine = log[i].trim();
-            
+        for (final String logLine : log) {
+            final String nextLogLine = logLine.trim();
+
             if (filterMatcher != null) {
-                filterMatcher.reset(nextLogLine);
-                if (!filterMatcher.find()) continue;
+            	filterMatcher.reset(nextLogLine);
+            	if (!filterMatcher.find()) continue;
             }
-            
-            if (nextLogLine.startsWith("E ")) level = 4;
-            else if (nextLogLine.startsWith("W ")) level = 3;
-            else if (nextLogLine.startsWith("S ")) level = 2;
-            else if (nextLogLine.startsWith("I ")) level = 1;
-            else if (nextLogLine.startsWith("D ")) level = 0;            
+
+            if (nextLogLine.startsWith("E ")) {
+                level = 4;
+            } else if (nextLogLine.startsWith("W ")) {
+                level = 3;
+            } else if (nextLogLine.startsWith("S ")) {
+                level = 2;
+            } else if (nextLogLine.startsWith("I ")) {
+                level = 1;
+            } else if (nextLogLine.startsWith("D ")) {
+                level = 0;
+            }
+
             prop.put("log_" + lc + "_level", level);
-            if (json) prop.putJSON("log_" + lc + "_line", nextLogLine);
-            else prop.put("log_" + lc + "_line", nextLogLine);
+
+            if (json) {
+                prop.putJSON("log_" + lc + "_line", nextLogLine);
+            } else {
+                prop.putHTML("log_" + lc + "_line", nextLogLine);
+            }
+
             lc++;
         }
         prop.put("log", lc);
