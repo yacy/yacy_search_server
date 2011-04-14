e40262258414111fa96d85c12c929fc1fd88f1c5
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2011-04-14 20:12:14 +0000

    removed httpclient-3.1 (this was added with last commit which was a mistake) the httpclient is required by solrj but no class from solrj is used which references to httpclient-3.1 Instead the YaCy http client library based on the apache http client 4.1 is used using a wrapper class which is in net.yacy.cora.services.federated.solr.SolrHTTPClient
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@7655 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/net/yacy/cora/services/federated/solr/SolrSingleConnector.java
+++ source/net/yacy/cora/services/federated/solr/SolrSingleConnector.java
@@ -1,3 +1,27 @@
+/**
+ *  SolrSingleConnector
+ *  Copyright 2011 by Michael Peter Christen
+ *  First released 14.04.2011 at http://yacy.net
+ *
+ *  $LastChangedDate: 2011-04-14 22:05:04 +0200 (Do, 14 Apr 2011) $
+ *  $LastChangedRevision: 7654 $
+ *  $LastChangedBy: orbiter $
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.yacy.cora.services.federated.solr;
 
 import java.io.File;
