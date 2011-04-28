528da7c9ea29e7fd78b578eec62812a3394688e0
orbiter <orbiter@6c8d7289-2bf4-0310-a012-ef5d649a1542>
2011-04-28 13:14:30 +0000

    removed unused class and added license header for new class
    git-svn-id: https://svn.berlios.de/svnroot/repos/yacy/trunk@7680 6c8d7289-2bf4-0310-a012-ef5d649a1542

--- source/net/yacy/document/parser/html/Evaluation.java
+++ source/net/yacy/document/parser/html/Evaluation.java
@@ -1,3 +1,27 @@
+/**
+ *  Evaluation
+ *  Copyright 2011 by Michael Peter Christen
+ *  First released 28.04.2011 at http://yacy.net
+ *
+ *  $LastChangedDate: 2011-03-08 02:51:51 +0100 (Di, 08 Mrz 2011) $
+ *  $LastChangedRevision: 7567 $
+ *  $LastChangedBy: low012 $
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.yacy.document.parser.html;
 
 import java.io.File;
