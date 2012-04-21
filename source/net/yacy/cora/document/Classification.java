8a08c96a8262e94c79f68359bf3dc8ada0dadcdf
Michael Peter Christen <mc@yacy.net>
2012-04-21 21:32:31 +0200

    removed dependency from logging

--- source/net/yacy/cora/document/Classification.java
+++ source/net/yacy/cora/document/Classification.java
@@ -7,12 +7,12 @@
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- *  
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program in the file lgpl21.txt
  *  If not, see <http://www.gnu.org/licenses/>.
@@ -27,8 +27,6 @@ import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
-import net.yacy.kelondro.logging.Log;
-
 public class Classification {
 
     private static final Set<String> mediaExtSet = new HashSet<String>();
@@ -36,22 +34,22 @@ public class Classification {
     private static final Set<String> audioExtSet = new HashSet<String>();
     private static final Set<String> videoExtSet = new HashSet<String>();
     private static final Set<String> appsExtSet = new HashSet<String>();
-    
-    
+
+
     static {
-        
+
         final String apps = "7z,ace,arc,arj,apk,asf,asx,bat,bin,bkf,bz2,cab,com,css,dcm,deb,dll,dmg,exe,gho,ghs,gz,hqx,img,iso,jar,lha,rar,sh,sit,sitx,tar,tbz,tgz,tib,torrent,vbs,war,zip";
         final String audio = "aac,aif,aiff,flac,m4a,m4p,mid,mp2,mp3,oga,ogg,ram,sid,wav,wma";
         final String video = "3g2,3gp,3gp2,3gpp,3gpp2,3ivx,asf,asx,avi,div,divx,dv,dvx,env,f4v,flv,hdmov,m1v,m4v,m-jpeg,moov,mov,movie,mp2v,mp4,mpe,mpeg,mpg,mpg4,mv4,ogm,ogv,qt,rm,rv,vid,swf,wmv";
         final String image = "ai,bmp,cdr,cmx,emf,eps,gif,img,jpeg,jpg,mng,pct,pdd,pdn,pict,png,psb,psd,psp,tif,tiff,wmf";
-        
+
         addSet(imageExtSet, image); // image formats
         addSet(audioExtSet, audio); // audio formats
         addSet(videoExtSet, video); // video formats
         addSet(appsExtSet, apps);   // application formats
         addSet(mediaExtSet, apps + "," + audio + "," + video + "," + image); // all media formats
     }
-    
+
     private static void addSet(Set<String> set, final String extString) {
         if ((extString == null) || (extString.length() == 0)) return;
         for (String s: extString.split(",")) set.add(s.toLowerCase().trim());
@@ -81,7 +79,7 @@ public class Classification {
         if (appsExt == null) return false;
         return appsExtSet.contains(appsExt.trim().toLowerCase());
     }
-    
+
     public static boolean isPictureMime(final String mimeType) {
         if (mimeType == null) return false;
         return mimeType.toUpperCase().startsWith("IMAGE");
@@ -89,7 +87,7 @@ public class Classification {
 
 
     private static final Properties mimeTable = new Properties();
-    
+
     public static void init(final File mimeFile) {
         if (mimeTable.isEmpty()) {
             // load the mime table
@@ -97,30 +95,29 @@ public class Classification {
             try {
                 mimeTableInputStream = new BufferedInputStream(new FileInputStream(mimeFile));
                 mimeTable.load(mimeTableInputStream);
-            } catch (final Exception e) {                
-                Log.logException(e);
+            } catch (final Exception e) {
             } finally {
-                if (mimeTableInputStream != null) try { mimeTableInputStream.close(); } catch (final Exception e1) {}                
+                if (mimeTableInputStream != null) try { mimeTableInputStream.close(); } catch (final Exception e1) {}
             }
         }
     }
-    
+
     public static int countMimes() {
         return mimeTable.size();
     }
-    
+
     public static String ext2mime(final String ext) {
         return mimeTable.getProperty(ext, "application/" + ext);
     }
-    
+
     public static String ext2mime(final String ext, final String dfltMime) {
         return mimeTable.getProperty(ext, dfltMime);
     }
-    
+
     public static String url2mime(final MultiProtocolURI url, final String dfltMime) {
         return ext2mime(url.getFileExtension(), dfltMime);
     }
-    
+
     public static String url2mime(final MultiProtocolURI url) {
         return ext2mime(url.getFileExtension());
     }
