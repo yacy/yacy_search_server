e859481889cf265cdd78e53431e6cf05741fcd9e
cominch <dominic@luetjohann.com>
2012-06-10 10:55:00 +0200

    Add Triplestore settings functionality
    Conflicts:
    	htroot/env/templates/header.template

---
+++ htroot/Triplestore_p.java
@@ -0,0 +1,38 @@
+import net.yacy.cora.protocol.RequestHeader;
+import de.anomic.server.serverObjects;
+import de.anomic.server.serverSwitch;
+
+public final class Triplestore_p {
+
+	public static serverObjects respond(final RequestHeader header,
+			final serverObjects post, final serverSwitch env) {
+		// return variable that accumulates replacements
+		final serverObjects prop = new serverObjects();
+
+		if (post != null) {
+
+			if (post.containsKey("tsSettings")) {
+
+				env.setConfig("triplestore.persistent",
+						"on".equals(post.get("tspersistentenabled")) ? true : false);
+				
+//				env.setConfig("interaction.feedback.accept",
+//						"on".equals(post.get("acceptfeedbackenabled")) ? true : false);
+
+			}
+
+
+		}
+
+		prop.put("tspersistentenabled_checked",
+				env.getConfigBool("triplestore.persistent", false) ? "1" : "0");
+		
+//		prop.put("acceptfeedbackenabled_checked",
+//				env.getConfigBool("interaction.feedback.accept", false) ? "1" : "0");
+
+
+		// return rewrite properties
+		return prop;
+	}
+
+}
