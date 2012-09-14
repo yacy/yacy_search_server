2f218df55d869fd89f34f4d00de1653ae58aa3ef
Michael Peter Christen <mc@yacy.net>
2012-09-14 12:06:06 +0200

    added missing license headers

--- htroot/Triple_p.java
+++ htroot/Triple_p.java
@@ -1,3 +1,22 @@
+/**
+ *  Triple_p
+ *  Copyright 2012 by Michael Peter Christen, mc@yacy.net, Frankfurt am Main, Germany
+ *  First released 14.09.2011 at http://yacy.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -18,7 +37,6 @@ import com.hp.hpl.jena.query.ResultSetFormatter;
 import de.anomic.http.server.HTTPDemon;
 import de.anomic.server.serverObjects;
 import de.anomic.server.serverSwitch;
-
 
 public class Triple_p {
 
