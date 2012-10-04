0f7a54452d67c141d78d65f3ae4b404629c6dada
orbiter <mc@yacy.net>
2012-10-04 14:46:40 +0200

    fix for location search query encoding

--- source/net/yacy/search/query/QueryParams.java
+++ source/net/yacy/search/query/QueryParams.java
@@ -522,7 +522,8 @@ public final class QueryParams {
 
         if (this.radius > 0.0d && this.lat != 0.0d && this.lon != 0.0d) {
             // localtion search, no special ranking
-            q.append('&').append(CommonParams.FQ).append("={!bbox sfield=").append(YaCySchema.coordinate_p.name()).append("}&pt=");
+            // try http://localhost:8090/solr/select?q=*:*&fq={!bbox}&sfield=coordinate_p&pt=50.17,8.65&d=1
+            q.append('&').append(CommonParams.FQ).append("={!bbox}&sfield=").append(YaCySchema.coordinate_p.name()).append("&pt=");
             q.append(Double.toString(this.lat)).append(',').append(Double.toString(this.lon)).append("&d=").append(GeoLocation.degreeToKm(this.radius));
         } else {
             // set ranking
