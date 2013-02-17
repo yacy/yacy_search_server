1951ba61ae7f59981a6b9e7546552b6b233b4cc8
reger <reger18@arcor.de>
2013-02-17 03:26:46 +0100

    remove CPGEN from Windows batch files (classpath for all needed libraries is defined in manifest  of yacycore.jar)

--- stopYACY.bat
+++ stopYACY.bat
@@ -1,21 +1,10 @@
 @Echo Off
 title YaCy
-If %1.==CPGEN. GoTo :CPGEN
 
-Rem Generating the proper classpath unsing loops and labels
-Set CLASSPATH=classes;htroot
-For %%X in (lib/*.jar) Do Call %0 CPGEN lib\%%X
-For %%X in (libx/*.jar) Do Call %0 CPGEN libx\%%X
+Rem Setting the classpath
+Set CLASSPATH=classes;lib\yacycore.jar;htroot
 
 Rem Stopping yacy
 Echo Generated Classpath:%CLASSPATH%
 java -classpath %CLASSPATH% net.yacy.yacy -shutdown
 
-GoTo :END
-
-Rem This target is used to concatenate the classpath parts 
-:CPGEN
-Set CLASSPATH=%CLASSPATH%;%2
-
-Rem Target needed to jump to the end of the file
-:END
