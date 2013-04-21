c1a2175fbcf2b489ba7eb709f4c2f1a68de3c0e4
Michael Peter Christen <mc@yacy.net>
2013-04-21 12:29:05 +0200

    added transparency to gif image animation and the integration to the YaCy httpd for on-the-fly generated gifs (including animated gifs)

--- source/net/yacy/peers/graphics/EncodedImage.java
+++ source/net/yacy/peers/graphics/EncodedImage.java
@@ -1,15 +1,50 @@
+/**
+ *  EncodedImage
+ *  Copyright 2010 by Michael Christen
+ *  First released 28.07.2010 at http://yacy.net
+ *  
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.yacy.peers.graphics;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import net.yacy.kelondro.util.ByteBuffer;
+import net.yacy.visualization.AnimationGIF;
 import net.yacy.visualization.RasterPlotter;
 
 public class EncodedImage {
     private ByteBuffer image;
     private String extension;
-    
+
     public EncodedImage(final RasterPlotter sourceImage, final String targetExt) {
         this.image = "png".equals(targetExt) ? sourceImage.exportPng() : RasterPlotter.exportImage(sourceImage.getImage(), targetExt);
         this.extension = targetExt;
+    }
+    
+    public EncodedImage(final AnimationGIF sourceImage) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            bos.write(sourceImage.get());
+            bos.close();
+        } catch (IOException e) {
+        }
+        this.image = new ByteBuffer(bos.toByteArray());
+        this.extension = "gif";
     }
     
     public ByteBuffer getImage() {
