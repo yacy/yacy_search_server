0d4efabaa8da43c8577c008b7a0e4ced4e4c440a
reger <reger18@arcor.de>
2013-10-13 17:56:53 +0200

    fix YaCy version string in proxy headers (config parameter vString not longer used)

--- source/net/yacy/server/http/HTTPDProxyHandler.java
+++ source/net/yacy/server/http/HTTPDProxyHandler.java
@@ -88,6 +88,7 @@ import net.yacy.document.parser.html.ContentTransformer;
 import net.yacy.document.parser.html.Transformer;
 import net.yacy.kelondro.io.ByteCountOutputStream;
 import net.yacy.kelondro.util.FileUtils;
+import net.yacy.peers.operation.yacyBuildProperties;
 import net.yacy.repository.Blacklist.BlacklistType;
 import net.yacy.search.Switchboard;
 import net.yacy.search.SwitchboardConstants;
@@ -1208,7 +1209,7 @@ public final class HTTPDProxyHandler {
             viaValue
             .append(httpVer).append(" ")
             .append(myAddress).append(" ")
-            .append("(YaCy ").append(sb.getConfig("vString", "0.0")).append(")");
+            .append("(YaCy ").append(yacyBuildProperties.getVersion()).append(")");
 
             // storing header back
             header.put(HeaderFramework.VIA, viaValue.toString());
@@ -1572,7 +1573,7 @@ public final class HTTPDProxyHandler {
             userAgentStr
             .append(browserUserAgent.substring(0,pos))
             .append("; YaCy ")
-            .append(sb.getConfig("vString","0.1"))
+            .append(yacyBuildProperties.getVersion())
             .append("; yacy.net")
             .append(browserUserAgent.substring(pos));
         } else {
