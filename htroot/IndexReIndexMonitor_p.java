15b2fad6a2dce6ceec644429cca29cb0369a5651
Michael Peter Christen <mc@yacy.net>
2014-07-02 14:56:34 +0200

    reverted latest change for reindexing because that works actually only for internal Solr indexes. This is mainly caused by the fact that an external Solr may be also a SolrCloud which do not support LukeRequests, which are needed to request the old Schema.

--- htroot/IndexReIndexMonitor_p.java
+++ htroot/IndexReIndexMonitor_p.java
@@ -37,7 +37,7 @@ public class IndexReIndexMonitor_p {
         prop.put("currentselectquery","");
         BusyThread bt = sb.getThread("reindexSolr");
         if (bt == null) {
-            if (post != null && post.containsKey("reindexnow")) {
+            if (post != null && post.containsKey("reindexnow") && sb.index.fulltext().connectedLocalSolr()) {
                 migration.reindexToschema(sb);
                 prop.put("querysize", "0");
                 prop.put("infomessage","reindex job started");
@@ -75,8 +75,13 @@ public class IndexReIndexMonitor_p {
             }            
         } else {
             prop.put("reindexjobrunning", 0);
-            prop.put("querysize", "is empty");
-            prop.put("infomessage", "no reindex job running");
+            if (sb.index.fulltext().connectedLocalSolr()) {
+                prop.put("querysize", "is empty");
+                prop.put("infomessage", "no reindex job running");
+            } else {
+                prop.put("querysize", "");
+                prop.putHTML("infomessage", "! reindex works only with embedded Solr index !");
+            }
         }
         // return rewrite properties
         return prop;
