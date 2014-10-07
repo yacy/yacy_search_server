e413beac048e28a1d96dd80e2fc8bdce9249da2b
Michael Peter Christen <mc@yacy.net>
2014-10-07 17:52:13 +0200

    fix for latest UPnP update

--- htroot/yacysearch_location.java
+++ htroot/yacysearch_location.java
@@ -121,7 +121,7 @@ public class yacysearch_location {
             String promoteSearchPageGreeting = env.getConfig(SwitchboardConstants.GREETING, "");
             if (env.getConfigBool(SwitchboardConstants.GREETING_NETWORK_NAME, false)) promoteSearchPageGreeting = env.getConfig("network.unit.description", "");
             String hostName = header.get("Host", Domains.LOCALHOST);
-            if (hostName.indexOf(':',0) == -1) hostName += ":" + env.getLocalPort("port", "8090");
+            if (hostName.indexOf(':',0) == -1) hostName += ":" + env.getLocalPort("port", 8090);
             final String originalquerystring = (post == null) ? "" : post.get("query", post.get("search", "")).trim(); // SRU compliance
             final boolean global = post.get("kml_resource", "local").equals("global");
 
