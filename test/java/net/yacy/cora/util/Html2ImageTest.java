b74cddc49cbd3ea6b6d5946c0c604152ed073692
reger <reger18@arcor.de>
2016-05-16 20:34:19 +0200

    upd to Jetty v9.2.16.v20160414 - exclude unused mime4j - remove unused yacy-cora build

--- test/java/net/yacy/cora/util/Html2ImageTest.java
+++ test/java/net/yacy/cora/util/Html2ImageTest.java
@@ -7,9 +7,17 @@ import java.util.List;
 import net.yacy.utils.translation.ExtensionsFileFilter;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 
 public class Html2ImageTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        // make sure DATA exists
+        File f = new File("test/DATA");
+        if (!f.exists()) f.mkdir();
+    }
 
     /**
      * Test of pdf2image method, of class Html2Image.
@@ -22,6 +30,7 @@ public class Html2ImageTest {
         extensions.add("pdf");
         FilenameFilter fileFilter = new ExtensionsFileFilter(extensions);
         String[] pdffiles = pd.list(fileFilter);
+        assertTrue("no pdf files in test/parsertest directory", pdffiles.length > 0);
 
         for (String pdffilename : pdffiles) {
             File pdffile = new File(pd, pdffilename);
