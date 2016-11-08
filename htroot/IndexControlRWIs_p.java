042c2868dff4ee8924a81e2c239e868920b3622c
reger <reger18@arcor.de>
2016-11-08 23:42:12 +0100

    del abandoned indexcleaner.html, servlet deleted with commit https://github.com/yacy/yacy_search_server/commit/3bcd9d622b1988e221d271bdb088fd1fbcd5e018 crawler servlet log warning line on failure in one of multiple urls (instead of exception msg) indexcontrolrwi skip not needed type conversion on ranking

--- htroot/IndexControlRWIs_p.java
+++ htroot/IndexControlRWIs_p.java
@@ -489,14 +489,14 @@ public class IndexControlRWIs_p {
             DigestURL url;
             URIMetadataNode entry;
             String us;
-            float rn = Float.MIN_VALUE;
+            long rn = Long.MIN_VALUE;
             while (!theSearch.rwiIsEmpty() && (entry = theSearch.pullOneFilteredFromRWI(false)) != null) {
                 url = entry.url();
                 if ( url == null ) {
                     continue;
                 }
                 us = url.toNormalform(true);
-                if ( rn == Float.MIN_VALUE ) {
+                if ( rn == Long.MIN_VALUE ) {
                     rn = entry.score();
                 }
                 prop.put("genUrlList_urlList_" + i + "_urlExists", "1");
@@ -507,7 +507,7 @@ public class IndexControlRWIs_p {
                 prop.putHTML("genUrlList_urlList_" + i + "_urlExists_urlString", us);
                 prop.put("genUrlList_urlList_" + i + "_urlExists_urlStringShort",
                     (us.length() > 40) ? (us.substring(0, 20) + "<br>" + us.substring(20, 40) + "...") : ((us.length() > 30) ? (us.substring(0, 20) + "<br>" + us.substring(20)) : us));
-                prop.putNum("genUrlList_urlList_" + i + "_urlExists_ranking", Float.toString(entry.score() - rn));
+                prop.putNum("genUrlList_urlList_" + i + "_urlExists_ranking", entry.score() - rn);
                 prop.putNum("genUrlList_urlList_" + i + "_urlExists_domlength", DigestURL.domLengthEstimation(entry.hash()));
                 prop.putNum("genUrlList_urlList_" + i + "_urlExists_tf", 1000.0 * entry.word().termFrequency());
                 prop.putNum("genUrlList_urlList_" + i + "_urlExists_authority", (theSearch.getOrder() == null) ? -1 : theSearch.getOrder().authority(ASCII.String(entry.hash(), 6, 6)));
