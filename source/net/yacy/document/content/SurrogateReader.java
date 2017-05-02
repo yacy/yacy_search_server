yacy/yacy_search_server

commit 9ad4d16829547927c4111ba84218e4884c6487d3

Add a responsHeader to the solr index export with a format identifier and export parameter (in accordance with response xml format) for easier format detection on import.

source/net/yacy/document/content/SurrogateReader.java
@@ -180,17 +180,23 @@ public class SurrogateReader extends DefaultHandler implements Runnable {
     }
     
     /**
+     * Check for format string in responseHeader "yacy.index.export.solr.xml"
+     * (introduced v1.92/9188 2017-04-30) or guess format by existing "<respons>"
+     * and "<result>" or "<doc>" tag in the first 1024 characters.
+     *
      * @return true when inputStream is likely to contain a rich and full-text Solr xml data dump (see IndexExport_p.html)
      */
 	private boolean isSolrDump() {
 		boolean res = false;
-		byte[] b = new byte[100];
+		byte[] b = new byte[1024];
 		int nbRead = -1;
 		try {
                     nbRead = this.inputStream.read(b);
                     if (nbRead > 0) {
                         String s = new String(b, 0, nbRead, StandardCharsets.UTF_8);
-                        if ((s.contains("<response>") && s.contains("<result>")) || s.startsWith("<doc>")) {
+                        if (s.contains("format=\"yacy.index.export.solr.xml\"")) {
+                            res = true;
+                        } else if ((s.contains("<response>") && s.contains("<result>")) || s.startsWith("<doc>")) {
                             res = true;
                         }
                     }

source/net/yacy/document/content/DCEntry.java
@@ -176,7 +176,13 @@ public class DCEntry extends MultiMapSolrParams {
         }
     }
 
-    private static String bestU(String[] urls) {
+    /**
+     * From the given list of urls selects the best to access the resource,
+     * possibly expanding short identifier for doi nbn or itef with resolved url
+     * @param urls list of identifier / url
+     * @return best/resolved url
+     */
+    private String bestU(String[] urls) {
         if (urls.length > 1) { // with only one ... no choice
             for (String uu: urls) {
                 if (uu.startsWith("http://") && (uu.endsWith(".html") || uu.endsWith(".htm") || uu.endsWith(".pdf") || uu.endsWith(".doc") || uu.endsWith(".rss") || uu.endsWith(".xml"))) return uu;

source/net/yacy/search/index/Fulltext.java
@@ -803,6 +803,12 @@ public final class Fulltext {
                 if (this.format == ExportFormat.solr) {
                     pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
                     pw.println("<response>");
+                    pw.println("<lst name=\"responseHeader\">");
+                    pw.println(" <str format=\"yacy.index.export.solr.xml\"/>");
+                    pw.println(" <lst name=\"params\"");
+                    pw.println("  <str name=\"q\">" + this.query + "</str>");
+                    pw.println(" </lst>");
+                    pw.println("</lst>");
                     pw.println("<result>");
                 }
                 if (this.dom) {

commit 79fdf14b0abe85e74ae35cae44e0406b38f26559

Fixed regression introduced by commit 9ad4d16
On MediaWiki dump imports, the SurrogateReader was trying to unread too
many bytes, then failing with the following exception :
"java.io.IOException: Push back buffer is full".

source/net/yacy/document/content/SurrogateReader.java
@@ -71,6 +71,9 @@ public class SurrogateReader extends DefaultHandler implements Runnable {
     public final static String SURROGATES_MAIN_ELEMENT_CLOSE =
         "</" + SURROGATES_MAIN_ELEMENT_NAME + ">";
     public final static SolrInputDocument POISON_DOCUMENT = new SolrInputDocument();
+    
+    /** Maximum bytes number that can be unread on the underlying input stream */
+    private static final int PUSHBACK_SIZE = 1024;
 
     // class variables
     private final StringBuilder buffer;
@@ -100,7 +103,7 @@ public class SurrogateReader extends DefaultHandler implements Runnable {
     }
 
     public SurrogateReader(final InputStream stream, int queueSize, CrawlStacker crawlStacker, CollectionConfiguration configuration, int concurrency) throws IOException {
-        this(new PushbackInputStream(stream, 200), queueSize, crawlStacker, configuration, concurrency);
+        this(new PushbackInputStream(stream, PUSHBACK_SIZE), queueSize, crawlStacker, configuration, concurrency);
     }
     
     public SurrogateReader(final PushbackInputStream stream, int queueSize, CrawlStacker crawlStacker, CollectionConfiguration configuration, int concurrency) throws IOException {
@@ -181,14 +184,14 @@ public class SurrogateReader extends DefaultHandler implements Runnable {
     
     /**
      * Check for format string in responseHeader "yacy.index.export.solr.xml"
-     * (introduced v1.92/9188 2017-04-30) or guess format by existing "<respons>"
-     * and "<result>" or "<doc>" tag in the first 1024 characters.
+     * (introduced v1.92/9188 2017-04-30) or guess format by existing "<response>"
+     * and "<result>" or "<doc>" tag in the first {@value #PUSHBACK_SIZE} characters.
      *
      * @return true when inputStream is likely to contain a rich and full-text Solr xml data dump (see IndexExport_p.html)
      */
 	private boolean isSolrDump() {
 		boolean res = false;
-		byte[] b = new byte[1024];
+		byte[] b = new byte[PUSHBACK_SIZE];
 		int nbRead = -1;
 		try {
                     nbRead = this.inputStream.read(b);