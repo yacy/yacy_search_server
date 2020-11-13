804e88fa4b2a8ce5355e3dad415564db31e4364b
A984382 <72402052+A984382@users.noreply.github.com>
2020-11-13 21:24:58 +0100

    Unable importing library HttpCore
    HttpCore causes fail in building project by Maven, so it must be removed.

--- source/net/yacy/cora/protocol/http/auth/YaCyDigestSchemeFactory.java
+++ source/net/yacy/cora/protocol/http/auth/YaCyDigestSchemeFactory.java
@@ -23,8 +23,6 @@ package net.yacy.cora.protocol.http.auth;
 
 import java.nio.charset.Charset;
 
-import org.apache.http.annotation.Contract;
-import org.apache.http.annotation.ThreadingBehavior;
 import org.apache.http.auth.AuthScheme;
 import org.apache.http.auth.AuthSchemeFactory;
 import org.apache.http.auth.AuthSchemeProvider;
@@ -36,7 +34,6 @@ import org.apache.http.protocol.HttpContext;
  * {@link YaCyDigestScheme} instances that support use of YaCy encoded password
  * instead of clear-text password.
  */
-@Contract(threading = ThreadingBehavior.IMMUTABLE)
 public class YaCyDigestSchemeFactory implements AuthSchemeFactory, AuthSchemeProvider {
 
     private final Charset charset;
