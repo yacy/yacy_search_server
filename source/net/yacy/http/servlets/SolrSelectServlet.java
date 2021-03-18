8e4d014c0607a9bd61004d68777d5358a253d71b
sgaebel <sgaebel@localhost.localdomain>
2021-03-18 22:05:15 +0100

    removes useless SolrRequestInfo.clearRequestInfo(), avoids spamming the log

--- source/net/yacy/http/servlets/SolrSelectServlet.java
+++ source/net/yacy/http/servlets/SolrSelectServlet.java
@@ -73,7 +73,6 @@ import org.apache.solr.common.util.SimpleOrderedMap;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.request.SolrQueryRequestBase;
-import org.apache.solr.request.SolrRequestInfo;
 import org.apache.solr.response.BinaryResponseWriter;
 import org.apache.solr.response.CSVResponseWriter;
 import org.apache.solr.response.QueryResponseWriter;
@@ -381,7 +380,6 @@ public class SolrSelectServlet extends HttpServlet {
             if (req != null) {
                 req.close();
             }
-            SolrRequestInfo.clearRequestInfo();
             if (out != null) {
                 try {
                     out.close();
